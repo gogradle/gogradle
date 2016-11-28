1b607add0432997c6ac7ad648c857405631d5f7b
zhangbo <zhangbo@nanchao.org>
2016-11-28 15:27:05 +0800

    GolangPlugin and its test

---
+++ src/main/groovy/com/github/blindpirate/golang/plugin/GolangPlugin.groovy
@@ -0,0 +1,10 @@
+package com.github.blindpirate.golang.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GolangPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+    }
+}
