0631046481acc7533b9dfd9bf01f6676736d855b
zhangbo <zhangbo@nanchao.org>
2016-12-10 13:03:22 +0800

    refine GogradleRunner with stack

--- src/test/groovy/com/github/blindpirate/gogradle/GogradleRunner.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/GogradleRunner.groovy
@@ -1,5 +1,6 @@
 package com.github.blindpirate.gogradle
 
+import com.github.blindpirate.gogradle.util.FileUtils
 import com.github.blindpirate.gogradle.util.ReflectionUtils
 import net.lingala.zip4j.core.ZipFile
 import org.gradle.api.Project
@@ -8,10 +9,8 @@ import org.junit.runner.notification.RunNotifier
 import org.junit.runners.BlockJUnit4ClassRunner
 import org.junit.runners.model.FrameworkMethod
 import org.junit.runners.model.InitializationError
-import org.junit.runners.model.Statement
 import org.mockito.MockitoAnnotations
 
-import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
@@ -19,19 +18,29 @@ import static com.github.blindpirate.gogradle.util.FileUtils.forceDelete
 
 /**
  * <ul>
- *  <li>1.Check the usage of {@link WithProject} and inject {@link Project} instance if necessary.</li>
+ *  <li>1.Check the usage of {@link WithProject} and create {@link Project} currentInstance if necessary.</li>
  *  <li>2.Check the usage of {@link WithResource} and copy (or unzip) resources to temp directory if necessary.</li>
- *  <li>3.Inject fields annotated with{@link @Mock} and {@link @InjectMocks}.</li>
- *  <li>4.Clean up temp directories.</li>
+ *  <li>3.Inject the project currentInstance and resource directory into the test class currentInstance.</li>
+ *  <li>4.Inject fields annotated with{@link @Mock} and {@link @InjectMocks}.</li>
+ *  <li>5.Clean up temp directories whenever necessary.</li>
  * <ul>
  */
 // TODO: ignore @AccessWeb when offline
 public class GogradleRunner extends BlockJUnit4ClassRunner {
-    Object instance
 
-    File tmpProjectFolder = tmpRandomDirectory('project')
-    File tmpUserhomeFolder = tmpRandomDirectory('userhome')
-    File tmpResourceFolder = tmpDirectory('resource')
+    Object currentInstance
+
+    // Every time we find a @WithProject, a new temp project folder,a new user home folder and
+    // a new project currentInstance will
+    // be created then pushed into corresponding stack.
+    // At the end of that method, these resources will be popped and destroyed
+    ArrayDeque<File> tmpProjectDirStack = new ArrayDeque<>()
+    ArrayDeque<File> tmpUserhomeDirStack = new ArrayDeque<>()
+    ArrayDeque<Project> projectStack = new ArrayDeque<>()
+
+    // Every time we find a @WithResource, that resource will be copyed(or unzipped) to a temp dir
+    // At the end of that method, these resource will be popped and destroyed
+    ArrayDeque<File> tmpResourceDirStack = new ArrayDeque<>()
 
     public GogradleRunner(Class<?> klass) throws InitializationError {
         super(klass);
@@ -39,100 +48,120 @@ public class GogradleRunner extends BlockJUnit4ClassRunner {
 
     @Override
     public Object createTest() throws Exception {
-        instance = super.createTest();
-        MockitoAnnotations.initMocks(instance);
-        processWithProjectOnClass(instance);
-        processWithResourceOnClass(instance)
-        return instance;
+        currentInstance = super.createTest()
+        MockitoAnnotations.initMocks(currentInstance)
+        injectProjectAndResourceIfNecessary()
+        return currentInstance;
     }
 
-    @Override
-    protected Statement classBlock(RunNotifier notifier) {
-        cleanUp()
-        super.classBlock(notifier)
-    }
+    def injectProjectAndResourceIfNecessary() {
+        if (!projectStack.isEmpty()) {
+            ReflectionUtils.setField(currentInstance, 'project', projectStack.last)
+        }
 
-    def processWithResourceOnClass(Object instance) {
-        WithResource anno = instance.class.getAnnotation(WithResource)
-        if (anno) {
-            String resourceName = anno.value();
-            if (resourceName.endsWith('zip')) {
-                unzipResourceToTmpDir(resourceName)
-            } else {
-                copyResourceToTmpDir(resourceName)
-            }
+        if (!tmpResourceDirStack.isEmpty()) {
+            ReflectionUtils.setField(currentInstance, 'resource', tmpResourceDirStack.last)
         }
     }
 
-    def copyResourceToTmpDir(String resourceName) {
-        Path source = Paths.get(this.class.classLoader.getResource(resourceName).toURI())
-        Path dest = tmpResourceFolder.toPath()
-        Files.copy(source, dest)
+    void popResourceFromStack() {
+        File tmpResourceDir = tmpResourceDirStack.pop()
+        forceDelete(tmpResourceDir)
     }
 
-    def unzipResourceToTmpDir(String resourceName) {
+    Object popProjectFromStack() {
+        File tmpProjectDir = tmpProjectDirStack.pop()
+        File tmpUserhomeDir = tmpUserhomeDirStack.pop()
+        projectStack.pop()
+
+        forceDelete(tmpProjectDir)
+        forceDelete(tmpUserhomeDir)
+    }
+
+    File pushResourceToStack(String resourceName) {
+        File destDir = tmpRandomDirectory("resource");
+        if (resourceName.endsWith('zip')) {
+            unzipResourceToDir(resourceName, destDir)
+        } else {
+            copyResourceToDir(resourceName, destDir)
+        }
+
+        tmpResourceDirStack.push(destDir)
+    }
+
+    def copyResourceToDir(String resourceName, File destDir) {
+        Path source = Paths.get(this.class.classLoader.getResource(resourceName).toURI())
+        FileUtils.copyDirectory(source.toFile(), destDir)
+    }
+
+    def unzipResourceToDir(String resourceName, File destDir) {
         URI uri = this.class.classLoader.getResource(resourceName).toURI()
         ZipFile zipFile = new ZipFile(new File(uri))
-        zipFile.extractAll(tmpResourceFolder.path)
+        zipFile.extractAll(destDir.toString())
     }
 
+    def pushProjectToStack() {
+        File tmpProjectDir = tmpRandomDirectory('project')
+        File tmpUserhomeDir = tmpRandomDirectory('userhome')
+        Project project = new ProjectBuilderImpl().createProject('test', tmpProjectDir, tmpUserhomeDir)
 
-    def processWithProjectOnClass(Object instance) {
-        WithProject anno = instance.class.getAnnotation(WithProject);
-        if (anno) {
-            ReflectionUtils.setField(instance, 'project', newProject());
-        }
+        tmpProjectDirStack.push(tmpProjectDir)
+        tmpUserhomeDirStack.push(tmpUserhomeDir)
+        projectStack.push(project)
     }
 
     @Override
     protected void runChild(FrameworkMethod method, RunNotifier notifier) {
-        processWithProjectOnMethod(method)
-        processWithResourceOnMethod(method)
+        beforeOneTest(method)
 
         super.runChild(method, notifier);
+
+        afterOneTest(method);
     }
 
-    def processWithResourceOnMethod(FrameworkMethod frameworkMethod) {
-        WithResource anno = frameworkMethod.getAnnotation(WithResource);
-        if (anno) {
-            String resourceName = anno.value();
-            if (resourceName.endsWith('zip')) {
-                unzipResourceToTmpDir(resourceName)
-            } else {
-                copyResourceToTmpDir(resourceName)
-            }
+    void afterOneTest(FrameworkMethod method) {
+        WithResource withResource = findWithResource(method)
+        if (withResource) {
+            popResourceFromStack()
         }
-
-    }
-
-    def processWithProjectOnMethod(FrameworkMethod frameworkMethod) {
-        WithProject anno = frameworkMethod.getAnnotation(WithProject);
-        if (anno) {
-            ReflectionUtils.setField(instance, 'project', newProject());
+        WithProject withProject = findWithProject(method)
+        if (withProject) {
+            popProjectFromStack()
         }
     }
 
-    private Project newProject() {
-        new ProjectBuilderImpl()
-                .createProject("test", tmpProjectFolder, tmpUserhomeFolder);
+
+    def beforeOneTest(FrameworkMethod method) {
+        WithResource withResource = findWithResource(method);
+        if (withResource) {
+            pushResourceToStack(withResource.value())
+        }
+        WithProject withProject = findWithProject(method)
+        if (withProject) {
+            pushProjectToStack()
+        }
+    }
+
+    WithProject findWithProject(FrameworkMethod method) {
+        WithProject annoOnClass = method.method.declaringClass.getAnnotation(WithProject)
+        if (annoOnClass) {
+            return annoOnClass
+        }
+        return method.method.getAnnotation(WithProject)
+
+    }
+
+    WithResource findWithResource(FrameworkMethod method) {
+        WithResource annoOnClass = method.method.declaringClass.getAnnotation(WithResource)
+        if (annoOnClass) {
+            return annoOnClass
+        }
+        return method.method.getAnnotation(WithResource)
     }
 
     def tmpRandomDirectory(String prefix) {
-        File ret = new File("build/tmp/${prefix}-${UUID.randomUUID()}");
+        File ret = new File("build/tmp/${prefix}-${UUID.randomUUID()}")
         ret.mkdir()
         ret
-    }
-
-    def tmpDirectory(String name) {
-        File ret = new File("build/tmp/${name}")
-        ret.mkdir()
-        ret
-    }
-
-
-    public void cleanUp() {
-        forceDelete(tmpProjectFolder)
-        forceDelete(tmpUserhomeFolder)
-        forceDelete(tmpResourceFolder)
     }
 }
