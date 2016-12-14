aa3b8f7ff4995a4cdb470b5032602723d3db3f58
zhangbo <zhangbo@nanchao.org>
2016-12-14 18:25:28 +0800

    DirMapNotationParser and test

---
+++ src/main/java/com/github/blindpirate/gogradle/core/dependency/parse/DirMapNotationParser.java
@@ -0,0 +1,32 @@
+package com.github.blindpirate.gogradle.core.dependency.parse;
+
+import com.github.blindpirate.gogradle.core.dependency.GolangDependency;
+import com.github.blindpirate.gogradle.core.dependency.LocalDirectoryDependency;
+import com.github.blindpirate.gogradle.util.Assert;
+import com.github.blindpirate.gogradle.util.Cast;
+import com.github.blindpirate.gogradle.util.MapUtils;
+
+import java.util.Map;
+
+public class DirMapNotationParser extends AutoConfigureMapNotationParser {
+    private static final String PATH_KEY = "path";
+
+    @Override
+    public boolean accept(Object notation) {
+        return Cast.cast(Map.class, notation).containsKey(DIR_KEY);
+    }
+
+    @Override
+    protected void preConfigure(Map<String, Object> notationMap) {
+        super.preConfigure(notationMap);
+        Assert.isTrue(notationMap.containsKey(DIR_KEY), "Dir must be specified!");
+
+        notationMap.put(PATH_KEY, MapUtils.getString(notationMap, DIR_KEY));
+    }
+
+    @Override
+    protected Class<? extends GolangDependency> determinDependencyClass(Map<String, Object> notationMap) {
+        return LocalDirectoryDependency.class;
+    }
+
+}
