88e7575f87753166d0ab77279a7822a8cb645d04
zhangbo <zhangbo@nanchao.org>
2016-12-15 22:57:59 +0800

    task placeholders

---
+++ src/main/java/com/github/blindpirate/gogradle/core/task/BuildTask.java
@@ -0,0 +1,6 @@
+package com.github.blindpirate.gogradle.core.task;
+
+import org.gradle.api.DefaultTask;
+
+public class BuildTask extends DefaultTask{
+}
---
+++ src/main/java/com/github/blindpirate/gogradle/core/task/CleanTask.java
@@ -0,0 +1,12 @@
+package com.github.blindpirate.gogradle.core.task;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public class CleanTask extends DefaultTask {
+
+    @TaskAction
+    public void clean() {
+
+    }
+}
