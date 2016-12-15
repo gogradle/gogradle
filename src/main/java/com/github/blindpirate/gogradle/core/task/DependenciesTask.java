30029cc67c951fd1f8a88a390152958ba05493f1
zhangbo <zhangbo@nanchao.org>
2016-12-15 22:58:35 +0800

    incomplete task to print dependencies

--- src/main/java/com/github/blindpirate/gogradle/core/task/DependenciesTask.java
+++ src/main/java/com/github/blindpirate/gogradle/core/task/DependenciesTask.java
@@ -1,14 +1,24 @@
 package com.github.blindpirate.gogradle.core.task;
 
-import com.github.blindpirate.gogradle.GolangPlugin;
 import com.github.blindpirate.gogradle.core.dependency.produce.DependencyTreeNode;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
 
+import static com.github.blindpirate.gogradle.GolangPlugin.PREPARE_TASK_NAME;
+import static com.github.blindpirate.gogradle.GolangPlugin.RESOLVE_TASK_NAME;
+
+/**
+ * Prints the dependencies tree.
+ */
 public class DependenciesTask extends DefaultTask {
+
+    public DependenciesTask() {
+        dependsOn(RESOLVE_TASK_NAME);
+    }
+
     @TaskAction
     public void displayDependencies() {
-        ResolveTask task = (ResolveTask) getProject().getTasksByName(GolangPlugin.PREPARE_TASK_NAME, false);
+        ResolveTask task = (ResolveTask) getProject().getTasksByName(PREPARE_TASK_NAME, false);
         DependencyTreeNode tree = task.getDependencyTree();
         display(tree);
     }
