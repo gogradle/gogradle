e9465f6ac0dfe6002766ef7946d1030bebd0dbf7
zhangbo <zhangbo@nanchao.org>
2016-12-17 17:40:57 +0800

    add comment

--- src/main/java/com/github/blindpirate/gogradle/vcs/PackageFetcher.java
+++ src/main/java/com/github/blindpirate/gogradle/vcs/PackageFetcher.java
@@ -2,6 +2,9 @@ package com.github.blindpirate.gogradle.vcs;
 
 import java.nio.file.Path;
 
+/**
+ * Fetches a package specified by name.
+ */
 public interface PackageFetcher {
     String HTTPS = "https://";
     String HTTP = "http://";
