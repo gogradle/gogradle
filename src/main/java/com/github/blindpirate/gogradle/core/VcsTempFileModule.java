a7f0a43012e7d9f51703d9abec2ef63d6c6925b7
zhangbo <zhangbo@nanchao.org>
2016-12-17 17:37:49 +0800

    rename and add toNotation()

---
+++ src/main/java/com/github/blindpirate/gogradle/core/VcsTempFileModule.java
@@ -0,0 +1,32 @@
+package com.github.blindpirate.gogradle.core;
+
+import com.github.blindpirate.gogradle.core.dependency.LockEnabled;
+
+import java.nio.file.Path;
+import java.util.Map;
+
+/**
+ * Its updateTime is from vcs.
+ */
+public class VcsTempFileModule extends FileSystemModule implements LockEnabled {
+
+    private Map<String, String> lockNotation;
+
+    public VcsTempFileModule(String name,
+                             Path rootDir,
+                             long updateTime,
+                             Map<String, String> lockNotation) {
+        super(name, rootDir, updateTime);
+        this.lockNotation = lockNotation;
+    }
+
+    @Override
+    public GolangPackageModule vendor(Path relativePathToVendor) {
+        return null;
+    }
+
+    @Override
+    public Map<String, String> toLockNotation() {
+        return lockNotation;
+    }
+}
