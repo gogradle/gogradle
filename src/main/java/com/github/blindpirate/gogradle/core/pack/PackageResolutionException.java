79bacf2e6415ebeb10b2e8eb4f29ada1ca154221
zhangbo <zhangbo@nanchao.org>
2016-12-17 17:45:36 +0800

    new classes

---
+++ src/main/java/com/github/blindpirate/gogradle/core/pack/PackageResolutionException.java
@@ -0,0 +1,7 @@
+package com.github.blindpirate.gogradle.core.pack;
+
+public class PackageResolutionException extends RuntimeException {
+    public PackageResolutionException(String message) {
+        super(message);
+    }
+}
