7d70363903621dc6cb5e25788276878bb5c0e44c
zhangbo <zhangbo@nanchao.org>
2016-12-17 17:38:30 +0800

    close resource with try block

--- src/main/java/com/github/blindpirate/gogradle/util/ProcessUtils.java
+++ src/main/java/com/github/blindpirate/gogradle/util/ProcessUtils.java
@@ -3,16 +3,20 @@ package com.github.blindpirate.gogradle.util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 public class ProcessUtils {
     public static String getOutput(Process process) throws IOException, InterruptedException {
         process.waitFor();
-        BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        StringBuffer sb = new StringBuffer();
-        String line;
-        while ((line = br.readLine()) != null) {
-            sb.append(line).append("\n");
+        try (BufferedReader br =
+                     new BufferedReader(
+                             new InputStreamReader(process.getInputStream(), Charset.forName("UTF8")))) {
+            StringBuffer sb = new StringBuffer();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            return sb.toString();
         }
-        return sb.toString();
     }
 }
