20bc6e0c731fc08c59f7c70247f39469df315f78
zhangbo <zhangbo@nanchao.org>
2016-12-17 17:37:13 +0800

    suppress warnings

--- src/main/java/com/github/blindpirate/gogradle/core/dependency/DependencyHelper.java
+++ src/main/java/com/github/blindpirate/gogradle/core/dependency/DependencyHelper.java
@@ -4,11 +4,14 @@ import com.github.blindpirate.gogradle.core.GolangPackageModule;
 import com.github.blindpirate.gogradle.core.dependency.parse.NotationParser;
 import com.github.blindpirate.gogradle.core.dependency.resolve.DependencyFactory;
 import com.google.inject.Injector;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import java.util.Collection;
 
 public class DependencyHelper {
 
+    @SuppressWarnings({"checkstyle:staticvariablename", "checkstyle:visibilitymodifier"})
+    @SuppressFBWarnings("MS_SHOULD_BE_FINAL")
     public static Injector INJECTOR_INSTANCE;
 
     public static GolangDependencySet parseMany(Collection<?> notations, NotationParser parser) {
@@ -21,6 +24,7 @@ public class DependencyHelper {
         return ret;
     }
 
+    @SuppressFBWarnings("UWF_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD")
     public static GolangDependencySet produceDependencies(GolangPackageModule module) {
         return INJECTOR_INSTANCE.getInstance(DependencyFactory.class).produce(module);
     }
