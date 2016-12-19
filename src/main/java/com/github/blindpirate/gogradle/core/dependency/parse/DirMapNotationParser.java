1cdb377b92a65784236c1611b371c450032b3181
zhangbo <zhangbo@nanchao.org>
2016-12-20 07:20:57 +0800

    refactor to simplify DirMapNotationParser

--- src/main/java/com/github/blindpirate/gogradle/core/dependency/parse/DirMapNotationParser.java
+++ src/main/java/com/github/blindpirate/gogradle/core/dependency/parse/DirMapNotationParser.java
@@ -2,33 +2,14 @@ package com.github.blindpirate.gogradle.core.dependency.parse;
 
 import com.github.blindpirate.gogradle.core.dependency.GolangDependency;
 import com.github.blindpirate.gogradle.core.dependency.LocalDirectoryDependency;
-import com.github.blindpirate.gogradle.util.Assert;
-import com.github.blindpirate.gogradle.util.Cast;
-import com.github.blindpirate.gogradle.util.MapUtils;
 
 import javax.inject.Singleton;
 import java.util.Map;
 
 @Singleton
 public class DirMapNotationParser extends AutoConfigureMapNotationParser {
-    private static final String PATH_KEY = "path";
-
     @Override
-    public boolean accept(Object notation) {
-        return Cast.cast(Map.class, notation).containsKey(DIR_KEY);
-    }
-
-    @Override
-    protected void preConfigure(Map<String, Object> notationMap) {
-        super.preConfigure(notationMap);
-        Assert.isTrue(notationMap.containsKey(DIR_KEY), "Dir must be specified!");
-
-        notationMap.put(PATH_KEY, MapUtils.getString(notationMap, DIR_KEY));
-    }
-
-    @Override
-    protected Class<? extends GolangDependency> determinDependencyClass(Map<String, Object> notationMap) {
+    protected Class<? extends GolangDependency> determineDependencyClass(Map<String, Object> notationMap) {
         return LocalDirectoryDependency.class;
     }
-
 }
--- src/test/groovy/com/github/blindpirate/gogradle/core/dependency/parse/DirMapNotationParserTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/core/dependency/parse/DirMapNotationParserTest.groovy
@@ -1,27 +1,19 @@
 package com.github.blindpirate.gogradle.core.dependency.parse
 
-import com.github.blindpirate.gogradle.GogradleRunner
 import com.github.blindpirate.gogradle.core.dependency.GolangDependency
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(GogradleRunner)
 class DirMapNotationParserTest {
 
     DirMapNotationParser parser = new DirMapNotationParser()
 
     @Test
-    void 'notation without dir should be rejected'() {
-        assert !parser.accept([:])
-    }
-
-    @Test
     void 'notation with dir should be parsed correctly'() {
         // when
-        GolangDependency dependency = parser.parseMap([name: 'name', dir: 'dir'])
+        GolangDependency dependency = parser.parse([name: 'name', dir: 'dir'])
 
         // then
         assert dependency.name == 'name'
-        assert dependency.path == 'dir'
+        assert dependency.dir == 'dir'
     }
 }
