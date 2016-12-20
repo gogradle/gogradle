22b7306626641dc01692d367eeed504f7c0edbf0
zhangbo <zhangbo@nanchao.org>
2016-12-20 20:56:48 +0800

    GitDependency POJO

--- src/main/java/com/github/blindpirate/gogradle/core/dependency/GitDependency.java
+++ src/main/java/com/github/blindpirate/gogradle/core/dependency/GitDependency.java
@@ -3,21 +3,26 @@ package com.github.blindpirate.gogradle.core.dependency;
 import com.github.blindpirate.gogradle.core.pack.DependencyResolver;
 import com.github.blindpirate.gogradle.vcs.git.GitDependencyResolver;
 
+import java.util.List;
+
+// TODO what about branch?
 public class GitDependency extends AbstractNotationDependency {
 
     public static final String NEWEST_COMMIT = "NEWEST_COMMIT";
 
     public static final String URL_KEY = "url";
+    public static final String URLS_KEY = "urls";
     public static final String COMMIT_KEY = "commit";
+    // not implemented yet
     public static final String BRANCH_KEY = "branch";
     public static final String TAG_KEY = "tag";
-    private String url;
+    public static final String VERSION_KEY = "version";
     private String commit;
     private String tag;
-
-    public String getUrl() {
-        return url;
-    }
+    // url specified by user
+    private String url;
+    // urls in PackageInfo e.g https:// and git+ssh://
+    private List<String> urls;
 
     public String getCommit() {
         return commit;
@@ -27,30 +32,37 @@ public class GitDependency extends AbstractNotationDependency {
         return tag;
     }
 
-    public GitDependency setUrl(String url) {
-        this.url = url;
-        return this;
-    }
-
-    public GitDependency setCommit(String commit) {
+    public void setCommit(String commit) {
         this.commit = commit;
-        return this;
     }
 
-    public GitDependency setTag(String tag) {
+    public void setTag(String tag) {
         this.tag = tag;
-        return this;
+    }
+
+    public List<String> getUrls() {
+        return urls;
+    }
+
+    public void setUrls(List<String> urls) {
+        this.urls = urls;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setVersion(String version) {
+        this.tag = version;
     }
 
     @Override
     public String getVersion() {
         return tag;
-    }
-
-
-    public GitDependency setVersion(String version) {
-        this.tag = version;
-        return this;
     }
 
     @Override
