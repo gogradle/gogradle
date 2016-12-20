eaca8cb00c3a7fa5b7c1c85e0c3d1e55d2c2c6dd
zhangbo <zhangbo@nanchao.org>
2016-12-20 21:06:34 +0800

    add some util methods

--- src/main/java/com/github/blindpirate/gogradle/util/MapUtils.java
+++ src/main/java/com/github/blindpirate/gogradle/util/MapUtils.java
@@ -1,5 +1,8 @@
 package com.github.blindpirate.gogradle.util;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class MapUtils {
@@ -14,4 +17,23 @@ public class MapUtils {
     }
 
 
+    public static <T> T getValue(Map<String, Object> map, String key, Class<T> clazz) {
+        return (T) map.get(key);
+    }
+
+    public static <K, V> Map<K, V> asMap(K k1, V v1, K k2, V v2) {
+        return asMap(new Pair[]{Pair.of(k1, v1), Pair.of(k2, v2)});
+    }
+
+    private static <K, V> Map<K, V> asMap(Pair<K, V>... entries) {
+        Map<K, V> ret = new HashMap<>();
+        for (Pair<K, V> entry : entries) {
+            ret.put(entry.getLeft(), entry.getRight());
+        }
+        return ret;
+    }
+
+    public static <K, V> Map<K, V> asMap(K k, V v) {
+        return asMap(Pair.of(k, v));
+    }
 }
