1f988985cfa1333dc5ec60c1fb1b447c8b9595f0
zhangbo <zhangbo@nanchao.org>
2016-12-20 21:02:33 +0800

    remove classes after refactoring

--- src/main/java/com/github/blindpirate/gogradle/core/dependency/parse/StringNotationParser.java
+++
@@ -1,7 +0,0 @@
-package com.github.blindpirate.gogradle.core.dependency.parse;
-
-import com.github.blindpirate.gogradle.core.dependency.GolangDependency;
-
-public interface StringNotationParser extends NotationParser {
-    GolangDependency parseString(String notation);
-}
