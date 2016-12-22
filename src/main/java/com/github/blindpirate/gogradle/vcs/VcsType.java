c69541b8ae70491adc7644e819a9cb93c65cb8d1
zhangbo <zhangbo@nanchao.org>
2016-12-22 23:15:02 +0800

    refactor VcsType

--- src/main/java/com/github/blindpirate/gogradle/vcs/VcsType.java
+++ src/main/java/com/github/blindpirate/gogradle/vcs/VcsType.java
@@ -1,30 +1,34 @@
 package com.github.blindpirate.gogradle.vcs;
 
+import com.github.blindpirate.gogradle.core.dependency.parse.NotationConverter;
 import com.google.common.base.Optional;
 import com.google.inject.Injector;
 import com.google.inject.Key;
-import com.google.inject.name.Names;
+
+import java.lang.annotation.Annotation;
+import java.util.List;
+import java.util.Map;
 
 public enum VcsType {
-    Git("git"),
-    Mercurial("hg"),
-    Svn("svn"),
-    Bazaar("bzr");
+    Git("git", ".git", Git.class),
+    Mercurial("hg", ".hg", Mercurial.class),
+    Svn("svn", ".svn", Svn.class),
+    Bazaar("bzr", ".bzr", Bazaar.class);
 
     private String suffix;
 
     private static Injector injector;
+    private String repo;
+    private Class<? extends Annotation> annoClass;
 
     public static void setInjector(Injector injector) {
         VcsType.injector = injector;
     }
 
-    public PackageFetcher getFetcher() {
-        return injector.getInstance(Key.get(PackageFetcher.class, Names.named(toString())));
-    }
-
-    VcsType(String suffix) {
+    VcsType(String suffix, String repo, Class annoClass) {
         this.suffix = suffix;
+        this.repo = repo;
+        this.annoClass = annoClass;
     }
 
     public static Optional<VcsType> of(String name) {
@@ -36,12 +40,19 @@ public enum VcsType {
         return Optional.absent();
     }
 
-    public static Optional<VcsType> ofDotSuffix(String name) {
-        for (VcsType type : values()) {
-            if (name.endsWith("." + type.suffix)) {
-                return Optional.of(type);
-            }
-        }
-        return Optional.absent();
+    public <T> T getService(Class<T> serviceClass) {
+        return injector.getInstance(Key.get(serviceClass, annoClass));
+    }
+
+    public String getRepo() {
+        return repo;
+    }
+
+    public VcsAccessor getAccessor() {
+        return getService(VcsAccessor.class);
+    }
+
+    public NotationConverter getNotationConverter() {
+        return getService(NotationConverter.class);
     }
 }
--- src/test/groovy/com/github/blindpirate/gogradle/vcs/VcsTypeTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/vcs/VcsTypeTest.groovy
@@ -1,31 +1,19 @@
 package com.github.blindpirate.gogradle.vcs
 
-import com.github.blindpirate.gogradle.GogradleRunner
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Mock
 
 import static com.github.blindpirate.gogradle.vcs.VcsType.*
 
-@RunWith(GogradleRunner)
 class VcsTypeTest {
-    @Mock
-    PackageFetcher gitPackageFetcher
 
     @Test
     public void 'get vcs type by name should success'() {
-        assert of('git').get() == Git
-        assert of('hg').get() == Mercurial
-        assert of('svn').get() == Svn
-        assert of('bzr').get() == Bazaar
-
-        assert ofDotSuffix('.git').get() == Git
-        assert ofDotSuffix('.hg').get() == Mercurial
-        assert ofDotSuffix('.svn').get() == Svn
-        assert ofDotSuffix('.bzr').get() == Bazaar
+        assert of('git').get() == VcsType.Git
+        assert of('hg').get() == VcsType.Mercurial
+        assert of('svn').get() == VcsType.Svn
+        assert of('bzr').get() == VcsType.Bazaar
 
         assert !of('a').isPresent()
-        assert !ofDotSuffix('.a').isPresent()
     }
 
 }
