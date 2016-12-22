d4a08d179df4e74c6703aae021a4fb823b164b83
zhangbo <zhangbo@nanchao.org>
2016-12-22 23:21:56 +0800

    rewrite interface method

--- src/main/java/com/github/blindpirate/gogradle/core/pack/PackageNameResolver.java
+++ src/main/java/com/github/blindpirate/gogradle/core/pack/PackageNameResolver.java
@@ -1,7 +1,12 @@
 package com.github.blindpirate.gogradle.core.pack;
 
 
-import com.github.blindpirate.gogradle.general.PickyFactory;
+import com.github.blindpirate.gogradle.general.Factory;
+import com.google.common.base.Optional;
 
-public interface PackageNameResolver extends PickyFactory<VendorResolveContext, VendorPackageNameResolveResult> {
+public interface PackageNameResolver extends Factory<String, PackageInfo> {
+    String HTTP="http://";
+    String HTTPS="https://";
+    @Override
+    Optional<PackageInfo> produce(String packageName);
 }
