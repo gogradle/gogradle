674c067c5775725116921943010f5cb24722feaa
zhangbo <zhangbo@nanchao.org>
2016-12-24 15:45:53 +0800

    let cache contain root package when resolution ends

--- src/main/java/com/github/blindpirate/gogradle/core/pack/DefaultPackageNameResolver.java
+++ src/main/java/com/github/blindpirate/gogradle/core/pack/DefaultPackageNameResolver.java
@@ -2,15 +2,17 @@ package com.github.blindpirate.gogradle.core.pack;
 
 import com.github.blindpirate.gogradle.util.FactoryUtil;
 import com.github.blindpirate.gogradle.util.logging.DebugLog;
-import java.util.Optional;
 import com.google.inject.BindingAnnotation;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static java.lang.annotation.ElementType.FIELD;
@@ -26,24 +28,59 @@ public class DefaultPackageNameResolver implements PackageNameResolver {
     private final List<PackageNameResolver> delegates;
 
     @Inject
-    public DefaultPackageNameResolver(@PackageNameResolvers
-                                              List<PackageNameResolver> delegates) {
+    public DefaultPackageNameResolver(@PackageNameResolvers List<PackageNameResolver> delegates) {
         this.delegates = delegates;
     }
 
     @Override
     @DebugLog
     public Optional<PackageInfo> produce(String packageName) {
-        PackageInfo resultInCache = cache.get(packageName);
-        if (resultInCache != null) {
-            return Optional.of(resultInCache);
+        Optional<PackageInfo> resultFromCache = tryToFetchFromCache(packageName);
+        if (resultFromCache.isPresent()) {
+            return resultFromCache;
         }
         Optional<PackageInfo> result = FactoryUtil.produce(delegates, packageName);
-
         reportErrorIfResolutionFailed(packageName, result);
-
-        cache.put(packageName, result.get());
+        updateCache(packageName, result.get());
         return result;
+    }
+
+    private void updateCache(String packageName, PackageInfo packageInfo) {
+        cache.put(packageName, packageInfo);
+        if (packageInfo != PackageInfo.INCOMPLETE) {
+            cache.put(packageInfo.getRootName(), copyWithSameRoot(packageInfo, packageInfo.getRootName()));
+        }
+    }
+
+    private Optional<PackageInfo> tryToFetchFromCache(String packageName) {
+        PackageInfo exactMatch = cache.get(packageName);
+        if (exactMatch != null) {
+            return Optional.of(exactMatch);
+        }
+
+        Path path = Paths.get(packageName);
+        for (int i = 1; i < path.getNameCount(); ++i) {
+            Path current = path.subpath(0, i);
+            PackageInfo existingPackage = cache.get(current.toString());
+            if (isValid(existingPackage)) {
+                PackageInfo result = copyWithSameRoot(existingPackage, packageName);
+                return Optional.of(result);
+            }
+        }
+        return Optional.empty();
+    }
+
+    private PackageInfo copyWithSameRoot(PackageInfo anotherPackage, String packageName) {
+        return PackageInfo.builder()
+                .withName(packageName)
+                .withRootName(anotherPackage.getRootName())
+                .withVcsType(anotherPackage.getVcsType())
+                .withUrls(anotherPackage.getUrls())
+                .build();
+    }
+
+    private boolean isValid(PackageInfo existingPackage) {
+        return existingPackage != null && existingPackage != PackageInfo.INCOMPLETE;
     }
 
     private void reportErrorIfResolutionFailed(String packageName, Optional<PackageInfo> result) {
--- src/test/groovy/com/github/blindpirate/gogradle/core/pack/DefaultPackageNameResolverTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/core/pack/DefaultPackageNameResolverTest.groovy
@@ -1,14 +1,18 @@
 package com.github.blindpirate.gogradle.core.pack
 
 import com.github.blindpirate.gogradle.GogradleRunner
-import com.github.blindpirate.gogradle.core.exceptions.DependencyResolutionException
+import com.github.blindpirate.gogradle.util.ReflectionUtils
+import com.github.blindpirate.gogradle.vcs.VcsType
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 
+import static com.github.blindpirate.gogradle.util.ReflectionUtils.allFieldsEquals
+import static com.github.blindpirate.gogradle.util.ReflectionUtils.getField
 import static java.util.Optional.empty
 import static java.util.Optional.of
+import static org.mockito.ArgumentMatchers.anyString
 import static org.mockito.Mockito.*
 
 @RunWith(GogradleRunner)
@@ -17,6 +21,7 @@ class DefaultPackageNameResolverTest {
     PackageNameResolver resolver1
     @Mock
     PackageNameResolver resolver2
+
     @Mock
     PackageInfo packageInfo
 
@@ -29,6 +34,8 @@ class DefaultPackageNameResolverTest {
         resolver = new DefaultPackageNameResolver([resolver1, resolver2])
         when(resolver1.produce(packageName)).thenReturn(empty())
         when(resolver2.produce(packageName)).thenReturn(of(packageInfo))
+        when(packageInfo.getName()).thenReturn('package')
+        when(packageInfo.getRootName()).thenReturn('root')
     }
 
 
@@ -46,6 +53,32 @@ class DefaultPackageNameResolverTest {
         verify(resolver2, times(1)).produce(packageName)
     }
 
+    @Test
+    void 'root package should be put into cache after successful resolution'() {
+        // given
+        PackageInfo info = PackageInfo.builder()
+                .withName('github.com/a/b/c')
+                .withRootName('github.com/a/b')
+                .withVcsType(VcsType.Git)
+                .withUrls([])
+                .build()
+        when(resolver1.produce('github.com/a/b/c')).thenReturn(of(info))
+        // when
+        resolver.produce('github.com/a/b/c')
+        // then
+        assert ReflectionUtils.getField(resolver, 'cache').size() == 2
+    }
+
+    @Test
+    void 'root package should not be put into cache after resolution to incoplete package'() {
+        // given
+        when(resolver1.produce('gihub.com/a')).thenReturn(of(PackageInfo.INCOMPLETE))
+        // when
+        resolver.produce('gihub.com/a')
+        // then
+        assert ReflectionUtils.getField(resolver, 'cache').size() == 1
+    }
+
     @Test(expected = PackageResolutionException)
     void 'exception should be thrown if unable to resolve'() {
         // given
@@ -53,6 +86,34 @@ class DefaultPackageNameResolverTest {
 
         // then
         resolver.produce(packageName)
+    }
+
+    @Test
+    void 'root package result should be leveraged when resolving children package'() {
+        // given
+        PackageInfo rootInfo = PackageInfo.builder()
+                .withName('github.com/a/b')
+                .withRootName('github.com/a/b')
+                .withVcsType(VcsType.Git)
+                .withUrls([])
+                .build()
+        getField(resolver, 'cache').put('github.com', PackageInfo.INCOMPLETE)
+        getField(resolver, 'cache').put('github.com/a', PackageInfo.INCOMPLETE)
+        getField(resolver, 'cache').put('github.com/a/b', rootInfo)
+
+        // when
+        PackageInfo result1 = resolver.produce('github.com/a/b').get()
+        PackageInfo result2 = resolver.produce('github.com/a/b/c').get()
+        PackageInfo result3 = resolver.produce('github.com/a/b/c/d').get()
+
+        // then
+        verify(resolver1, times(0)).produce(anyString())
+        assert result1 == rootInfo
+        assert result2.name == 'github.com/a/b/c'
+        assert result3.name == 'github.com/a/b/c/d'
+        assert allFieldsEquals(result2, rootInfo, ['vcsType', 'urls', 'rootName'])
+        assert allFieldsEquals(result3, rootInfo, ['vcsType', 'urls', 'rootName'])
+
     }
 
 }
