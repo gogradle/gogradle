b98b08ba4d9e44270b3851e3735587f9659e1b97
zhangbo <zhangbo@nanchao.org>
2017-01-02 07:19:34 +0800

    add logic to BuildMode

--- src/main/java/com/github/blindpirate/gogradle/core/mode/BuildMode.java
+++ src/main/java/com/github/blindpirate/gogradle/core/mode/BuildMode.java
@@ -1,6 +1,29 @@
 package com.github.blindpirate.gogradle.core.mode;
 
+import com.github.blindpirate.gogradle.core.dependency.GolangDependencySet;
+
+import static com.github.blindpirate.gogradle.core.dependency.GolangDependencySet.merge;
+
 public enum BuildMode {
-    Develop,
-    Reproducible
+    Develop {
+        @Override
+        public GolangDependencySet determine(GolangDependencySet declaredDependencies,
+                                             GolangDependencySet vendorDependencies,
+                                             GolangDependencySet lockedDependencies) {
+            return merge(declaredDependencies, vendorDependencies, lockedDependencies);
+        }
+    },
+    Reproducible {
+        @Override
+        public GolangDependencySet determine(GolangDependencySet declaredDependencies,
+                                             GolangDependencySet vendorDependencies,
+                                             GolangDependencySet lockedDependencies) {
+            return merge(vendorDependencies, lockedDependencies, declaredDependencies);
+        }
+    };
+
+    public abstract GolangDependencySet determine(
+            GolangDependencySet declaredDependencies,
+            GolangDependencySet vendorDependencies,
+            GolangDependencySet lockedDependencies);
 }
