0bbe0d517fb769772139dec9f73f26906e400157
zhangbo <zhangbo@nanchao.org>
2017-01-02 08:06:54 +0800

    comment some code temporarily

--- src/main/java/com/github/blindpirate/gogradle/core/InjectionHelper.java
+++ src/main/java/com/github/blindpirate/gogradle/core/InjectionHelper.java
@@ -2,14 +2,11 @@ package com.github.blindpirate.gogradle.core;
 
 import com.github.blindpirate.gogradle.core.dependency.GolangDependencySet;
 import com.github.blindpirate.gogradle.core.dependency.parse.MapNotationParser;
-import com.github.blindpirate.gogradle.core.dependency.produce.DependencyProduceStrategy;
-import com.github.blindpirate.gogradle.core.dependency.resolve.DependencyFactory;
 import com.google.inject.Injector;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import java.util.Collection;
 import java.util.Map;
-import java.util.Optional;
 
 public class InjectionHelper {
 
@@ -25,12 +22,12 @@ public class InjectionHelper {
         return ret;
     }
 
-    @SuppressFBWarnings("UWF_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD")
-    public static Optional<GolangDependencySet> produceDependencies(GolangPackageModule module) {
-        return INJECTOR_INSTANCE.getInstance(DependencyFactory.class).produce(module);
-    }
-
-    public static <T extends DependencyProduceStrategy> T strategy(Class<T> clazz) {
-        return INJECTOR_INSTANCE.getInstance(clazz);
-    }
+//    @SuppressFBWarnings("UWF_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD")
+//    public static Optional<GolangDependencySet> produceDependencies(AbstractResolvedDependency module) {
+//        return INJECTOR_INSTANCE.getInstance(DependencyFactory.class).produce(module);
+//    }
+//
+//    public static <T extends DependencyProduceStrategy> T strategy(Class<T> clazz) {
+//        return INJECTOR_INSTANCE.getInstance(clazz);
+//    }
 }
