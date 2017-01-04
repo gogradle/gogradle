1a8d32a8d1608b078ce14f0c91df062112b00644
zhangbo <zhangbo@nanchao.org>
2017-01-04 21:24:45 +0800

    add getStrategy method to interface

--- src/main/java/com/github/blindpirate/gogradle/core/dependency/NotationDependency.java
+++ src/main/java/com/github/blindpirate/gogradle/core/dependency/NotationDependency.java
@@ -1,9 +1,13 @@
 package com.github.blindpirate.gogradle.core.dependency;
 
+import com.github.blindpirate.gogradle.core.dependency.produce.strategy.DependencyProduceStrategy;
 import org.gradle.api.specs.Spec;
 
 import java.util.Set;
 
 public interface NotationDependency extends GolangDependency {
+
+    DependencyProduceStrategy getStrategy();
+
     Set<Spec<GolangDependency>> getTransitiveDepExclusions();
 }
