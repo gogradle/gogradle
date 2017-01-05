ac0363c8a6c21187b5d06c06b6e4e9b24d2a3ee2
zhangbo <zhangbo@nanchao.org>
2017-01-05 21:40:37 +0800

    GoBinary entity and related interface

---
+++ src/main/java/com/github/blindpirate/gogradle/crossplatform/GoBinary.java
@@ -0,0 +1,22 @@
+package com.github.blindpirate.gogradle.crossplatform;
+
+public class GoBinary {
+    private String path;
+    private String version;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
--- src/main/java/com/github/blindpirate/gogradle/crossplatform/GoBinaryManager.java
+++ src/main/java/com/github/blindpirate/gogradle/crossplatform/GoBinaryManager.java
@@ -2,4 +2,6 @@ package com.github.blindpirate.gogradle.crossplatform;
 
 public interface GoBinaryManager {
     String binaryPath();
+
+    GoBinary getGoBinary();
 }
