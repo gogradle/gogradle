4fd161a1c1f99d037d47797a521cb942aa0f8fa5
zhangbo <zhangbo@nanchao.org>
2017-01-06 18:33:27 +0800

    remove DependencyInstaller

--- src/test/groovy/com/github/blindpirate/gogradle/task/TaskTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/task/TaskTest.groovy
@@ -3,7 +3,6 @@ package com.github.blindpirate.gogradle.task
 import com.github.blindpirate.gogradle.GolangPluginSetting
 import com.github.blindpirate.gogradle.build.BuildManager
 import com.github.blindpirate.gogradle.core.GolangTaskContainer
-import com.github.blindpirate.gogradle.core.dependency.DependencyInstaller
 import com.github.blindpirate.gogradle.core.dependency.produce.DependencyVisitor
 import com.github.blindpirate.gogradle.core.dependency.produce.strategy.GogradleRootProduceStrategy
 import com.github.blindpirate.gogradle.core.dependency.tree.DependencyTreeFactory
@@ -29,20 +28,16 @@ abstract class TaskTest {
     @Mock
     DependencyTreeFactory dependencyTreeFactory
     @Mock
-    DependencyInstaller dependencyInstaller
-    @Mock
     BuildManager buildManager
     // This is a real task container for test tasks to fetch notationDependency tasks from
     GolangTaskContainer golangTaskContainer = new GolangTaskContainer()
 
     @Before
     void superSetUp() {
-        for(Map.Entry entry in GolangTaskContainer.TASKS.entrySet())
-        GolangTaskContainer.TASKS.each { taskName, taskClass ->
-            long t0 = System.currentTimeMillis()
-            golangTaskContainer.put(taskClass, Mockito.mock(taskClass))
-            println(System.currentTimeMillis() - t0)
-        }
+        for (Map.Entry entry in GolangTaskContainer.TASKS.entrySet())
+            GolangTaskContainer.TASKS.each { taskName, taskClass ->
+                golangTaskContainer.put(taskClass, Mockito.mock(taskClass))
+            }
     }
 
     protected void injectTaskMembers(Task task) {
@@ -50,7 +45,6 @@ abstract class TaskTest {
         setFieldSafely(task, 'dependencyTreeFactory', dependencyTreeFactory)
         setFieldSafely(task, 'strategy', strategy)
         setFieldSafely(task, 'visitor', visitor)
-        setFieldSafely(task, 'dependencyInstaller', dependencyInstaller)
         setFieldSafely(task, 'golangTaskContainer', golangTaskContainer)
         setFieldSafely(task, 'buildManager', buildManager)
     }
