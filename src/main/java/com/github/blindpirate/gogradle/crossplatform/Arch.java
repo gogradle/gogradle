5b74791e887aadd0a66a1edba057a78ab887101e
zhangbo <zhangbo@nanchao.org>
2017-01-06 17:13:15 +0800

    complete os and arch auto detection

--- src/main/java/com/github/blindpirate/gogradle/crossplatform/Arch.java
+++ src/main/java/com/github/blindpirate/gogradle/crossplatform/Arch.java
@@ -1,6 +1,11 @@
 package com.github.blindpirate.gogradle.crossplatform;
 
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+
 // https://github.com/golang/go/blob/master/src/go/build/syslist.go
+// http://lopica.sourceforge.net/os.html
 // https://github.com/trustin/os-maven-plugin/blob/master/src/main/java/kr/motd/maven/os/Detector.java
 public enum Arch {
     I386 {
@@ -9,27 +14,48 @@ public enum Arch {
             return "386";
         }
     },
-    Amd64,
-    Amd64p32,
-    Arm,
-    Armbe,
-    Arm64,
-    Arm64be,
-    Ppc64,
-    Ppc64le,
-    Mips,
-    Mipsle,
-    Mips64,
-    Mips64le,
-    Mips64p32,
-    Mips64p32le,
-    Ppc,
+    AMD64,
+    AMD64P32,
+    ARM,
+    ARMBE,
+    ARM64,
+    ARM64BE,
+    PPC64,
+    PPC64LE,
+    MIPS,
+    MIPSLE,
+    MIPS64,
+    MIPS64LE,
+    MIPS64P32,
+    MIPS64P32LE,
+    PPC,
     S390,
-    S390x,
-    Sparc,
-    Sparc64;
+    S390X,
+    SPARC,
+    SPARC64;
 
     private static Arch hostArch;
+
+    private static final Map<String, Arch> ARCH_DETECTION_MAP = ImmutableMap.<String, Arch>builder()
+            .put("x86_64", AMD64)
+            .put("x64", AMD64)
+            .put("amd64", AMD64)
+            .put("x86_32", I386)
+            .put("x86", I386)
+            .put("x32", I386)
+            .put("i386", I386)
+            .put("i486", I386)
+            .put("i586", I386)
+            .put("i686", I386)
+            .put("arm", ARM)
+            .put("arm32", ARM)
+            .put("ppc", PPC)
+            .put("ppc32", PPC)
+            .put("ppc64", PPC64)
+            .put("ppc64le", PPC64LE)
+            .put("s390", S390)
+            .put("s390x", S390X)
+            .build();
 
     @Override
     public String toString() {
@@ -45,29 +71,8 @@ public enum Arch {
 
     private static Arch detectHostArch() {
         String arch = System.getProperty("os.arch");
-        if (arch.matches("^(x8664|amd64|ia32e|em64t|x64)$")) {
-            return Amd64;
-        }
-        if (arch.matches("^(x8632|x86|i[3-6]86|ia32|x32)$")) {
-            return I386;
-        }
-        if (arch.matches("^(arm|arm32)$")) {
-            return Arm;
-        }
-        if (arch.matches("^(ppc|ppc32)$")) {
-            return Ppc;
-        }
-        if ("ppc64".equals(arch)) {
-            return Ppc64;
-        }
-        if ("ppc64le".equals(arch)) {
-            return Ppc64le;
-        }
-        if ("s390".equals(arch)) {
-            return S390;
-        }
-        if ("s390x".equals(arch)) {
-            return S390x;
+        if (ARCH_DETECTION_MAP.containsKey(arch)) {
+            return ARCH_DETECTION_MAP.get(arch);
         }
         throw new IllegalStateException("Unrecognized architecture:" + arch);
     }
--- src/main/java/com/github/blindpirate/gogradle/crossplatform/Os.java
+++ src/main/java/com/github/blindpirate/gogradle/crossplatform/Os.java
@@ -1,20 +1,32 @@
 package com.github.blindpirate.gogradle.crossplatform;
 
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static org.apache.commons.lang3.SystemUtils.IS_OS_FREE_BSD;
+import static org.apache.commons.lang3.SystemUtils.IS_OS_LINUX;
+import static org.apache.commons.lang3.SystemUtils.IS_OS_MAC_OSX;
+import static org.apache.commons.lang3.SystemUtils.IS_OS_NET_BSD;
+import static org.apache.commons.lang3.SystemUtils.IS_OS_SOLARIS;
+import static org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS;
+import static org.apache.commons.lang3.SystemUtils.IS_OS_ZOS;
+
 // https://github.com/golang/go/blob/master/src/go/build/syslist.go
-// https://github.com/trustin/os-maven-plugin/blob/master/src/main/java/kr/motd/maven/os/Detector.java
 public enum Os {
-    Android,
-    Darwin,
-    Dragonfly,
-    Freebsd,
-    Linux,
-    Nacl,
-    Netbsd,
-    Openbsd,
-    Plan9,
-    Solaris,
-    Windows,
-    Zos;
+    ANDROID,
+    DARWIN,
+    DRAGONFLY,
+    FREEBSD,
+    LINUX,
+    NACL,
+    NETBSD,
+    OPENBSD,
+    PLAN9,
+    SOLARIS,
+    WINDOWS,
+    ZOS;
 
     public String toString() {
         return name().toLowerCase();
@@ -29,29 +41,25 @@ public enum Os {
         return hostOs;
     }
 
+    private static final Map<Os, Boolean> OS_DETECTION_MAP = ImmutableMap.<Os, Boolean>builder()
+            .put(LINUX, IS_OS_LINUX)
+            .put(WINDOWS, IS_OS_WINDOWS)
+            .put(DARWIN, IS_OS_MAC_OSX)
+            .put(FREEBSD, IS_OS_FREE_BSD)
+            .put(NETBSD, IS_OS_NET_BSD)
+            .put(SOLARIS, IS_OS_SOLARIS)
+            .put(ZOS, IS_OS_ZOS)
+            .build();
+
     private static Os detectOs() {
-        String os = System.getProperty("os.name");
-        if (os.startsWith("linux")) {
-            return Linux;
+        Optional<Map.Entry<Os, Boolean>> result = OS_DETECTION_MAP.entrySet()
+                .stream()
+                .filter(Map.Entry::getValue)
+                .findFirst();
+        if (result.isPresent()) {
+            return result.get().getKey();
         }
-        if (os.startsWith("macosx") || os.startsWith("osx")) {
-            return Darwin;
-        }
-        if (os.startsWith("freebsd")) {
-            return Freebsd;
-        }
-        if (os.startsWith("openbsd")) {
-            return Openbsd;
-        }
-        if (os.startsWith("netbsd")) {
-            return Netbsd;
-        }
-        if (os.startsWith("solaris") || os.startsWith("sunos")) {
-            return Solaris;
-        }
-        if (os.startsWith("windows")) {
-            return Windows;
-        }
-        throw new IllegalStateException("Unrecognized operation system:" + os);
+        throw new IllegalStateException("Unrecognized operation system:" + System.getProperty("os.name"));
     }
+
 }
