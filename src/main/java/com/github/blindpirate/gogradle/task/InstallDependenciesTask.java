899c28db1fdc8127fdc636e307fb86e2361946fc
zhangbo <zhangbo@nanchao.org>
2017-01-11 07:42:35 +0800

    move GolangTaskContaner to task package

--- src/main/java/com/github/blindpirate/gogradle/task/InstallDependenciesTask.java
+++ src/main/java/com/github/blindpirate/gogradle/task/InstallDependenciesTask.java
@@ -7,8 +7,9 @@ import org.gradle.api.tasks.TaskAction;
 
 import javax.inject.Inject;
 
-import static com.github.blindpirate.gogradle.core.GolangTaskContainer.RESOLVE_TASK_NAME;
+import static com.github.blindpirate.gogradle.task.GolangTaskContainer.RESOLVE_TASK_NAME;
 
+// TODO old dependencies should be deleted before installation
 public class InstallDependenciesTask extends AbstractGolangTask {
     @Inject
     private BuildManager buildManager;
@@ -18,7 +19,7 @@ public class InstallDependenciesTask extends AbstractGolangTask {
     }
 
     @TaskAction
-    void installDependencies() {
+    public void installDependencies() {
         DependencyTreeNode rootNode = getTask(ResolveTask.class).getDependencyTree();
         rootNode.flatten()
                 .stream()
