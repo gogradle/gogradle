abcc535cec173ee86c1b2666d9947fd2a3b992f9
zhangbo <zhangbodut@gmail.com>
2017-01-12 23:07:43 +0800

    Add getPackage method to NotationDependency interface

--- src/main/java/com/github/blindpirate/gogradle/core/dependency/NotationDependency.java
+++ src/main/java/com/github/blindpirate/gogradle/core/dependency/NotationDependency.java
@@ -1,11 +1,14 @@
 package com.github.blindpirate.gogradle.core.dependency;
 
+import com.github.blindpirate.gogradle.core.GolangPackage;
 import com.github.blindpirate.gogradle.core.dependency.produce.strategy.DependencyProduceStrategy;
 import org.gradle.api.specs.Spec;
 
 import java.util.Set;
 
 public interface NotationDependency extends GolangDependency {
+
+    GolangPackage getPackage();
 
     DependencyProduceStrategy getStrategy();
 
