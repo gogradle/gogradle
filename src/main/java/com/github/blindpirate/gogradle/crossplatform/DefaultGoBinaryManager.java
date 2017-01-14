b9a6a8f1c2761c2b92e1e0de968ed11bffc16c39
zhangbo <zhangbodut@gmail.com>
2017-01-14 21:11:22 +0800

    Default implementation of GoBinaryManager

--- src/main/java/com/github/blindpirate/gogradle/crossplatform/DefaultGoBinaryManager.java
+++ src/main/java/com/github/blindpirate/gogradle/crossplatform/DefaultGoBinaryManager.java
@@ -1,16 +1,173 @@
 package com.github.blindpirate.gogradle.crossplatform;
 
+import com.github.blindpirate.gogradle.GolangPluginSetting;
+import com.github.blindpirate.gogradle.core.cache.GlobalCacheManager;
+import com.github.blindpirate.gogradle.util.CompressUtils;
+import com.github.blindpirate.gogradle.util.ExceptionHandler;
+import com.github.blindpirate.gogradle.util.HttpUtils;
+import com.github.blindpirate.gogradle.util.IOUtils;
+import com.google.common.collect.ImmutableMap;
+
+import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.github.blindpirate.gogradle.util.ProcessUtils.ProcessResult;
+import static com.github.blindpirate.gogradle.util.ProcessUtils.getResult;
+import static com.github.blindpirate.gogradle.util.ProcessUtils.run;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
 @Singleton
 public class DefaultGoBinaryManager implements GoBinaryManager {
-    @Override
-    public String binaryPath() {
-        return "go";
+    // https://storage.googleapis.com/golang/go1.7.4.windows-386.zip
+    // https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz
+    private static final String URL
+            = "https://storage.googleapis.com/golang/go${version}.${os}-${arch}${extension}";
+    // http://golangtc.com/static/go/1.7.4/go1.7.4.windows-386.zip
+    // http://golangtc.com/static/go/1.7.4/go1.7.4.linux-amd64.tar.gz
+    private static final String URL_UNDER_GFW
+            = "http://golangtc.com/static/go/${version}/go${version}.${os}-${arch}${extension}";
+
+    private static final String FILENAME = "go${version}.${os}-${arch}${extension}";
+
+    private static final String GOBIN_RELATIVE = "go/bin/go";
+
+    private static final String NEWEST_VERSION_URL
+            = "http://gogradle.oss-cn-hongkong.aliyuncs.com/newest-stable-go-version.txt";
+
+    // go version go1.7.1 darwin/amd64
+    private static final Pattern GO_VERSION_OUTPUT_REGEX = Pattern.compile("go version go((\\d|\\.)+) .+");
+
+    private static Map<Boolean, String> urls = ImmutableMap.of(
+            TRUE, URL_UNDER_GFW,
+            FALSE, URL);
+
+
+    private final GolangPluginSetting setting;
+    private final GlobalCacheManager globalCacheManager;
+    private final HttpUtils httpUtils;
+
+    private boolean resolved = false;
+    private String binaryPath;
+    private String gorootEnv;
+
+    @Inject
+    public DefaultGoBinaryManager(GolangPluginSetting setting,
+                                  GlobalCacheManager globalCacheManager,
+                                  HttpUtils httpUtils) {
+        this.setting = setting;
+        this.globalCacheManager = globalCacheManager;
+        this.httpUtils = httpUtils;
     }
 
     @Override
-    public GoBinary getGoBinary() {
-        return null;
+    public String getBinaryPath() {
+        resolveIfNecessary();
+        return binaryPath;
+    }
+
+    @Override
+    public String getGorootEnv() {
+        resolveIfNecessary();
+        return gorootEnv;
+    }
+
+    private void resolveIfNecessary() {
+        if (resolved) {
+            return;
+        }
+        Optional<String> versionOnHost = goVersionOnHost();
+
+        if (setting.getGoVersion() != null) {
+            if (specificVersionIsHostVersion(versionOnHost)) {
+                binaryPath = "go";
+            } else {
+                fetchSpecifiedVersion(setting.getGoVersion());
+            }
+        } else {
+            if (versionOnHost.isPresent()) {
+                binaryPath = "go";
+            } else {
+                fetchNewestStableVersion();
+            }
+        }
+
+        resolved = true;
+    }
+
+    private boolean specificVersionIsHostVersion(Optional<String> versionOnHost) {
+        return setting.getGoVersion().equals(versionOnHost.orElse(null));
+    }
+
+    private void fetchNewestStableVersion() {
+        try {
+            String newestStableVersion = httpUtils.get(NEWEST_VERSION_URL).trim();
+            fetchSpecifiedVersion(newestStableVersion);
+        } catch (IOException e) {
+            throw ExceptionHandler.uncheckException(e);
+        }
+    }
+
+    private Optional<String> goVersionOnHost() {
+        try {
+            Process process = run("go", "version");
+            ProcessResult result = getResult(process);
+            Matcher m = GO_VERSION_OUTPUT_REGEX.matcher(result.getStdout());
+            if (m.find()) {
+                return Optional.of(m.group(1));
+            } else {
+                return Optional.empty();
+            }
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    private void fetchSpecifiedVersion(String version) {
+        Path goBinaryPath = globalCacheManager
+                .getGlobalGoBinCache(version)
+                .resolve(GOBIN_RELATIVE);
+        binaryPath = goBinaryPath.toAbsolutePath().toString();
+        if (!goBinaryPath.toFile().exists()) {
+            downloadSpecifiedVersion(version);
+        }
+    }
+
+    private void downloadSpecifiedVersion(String version) {
+        Path goRootPath = globalCacheManager.getGlobalGoBinCache(version);
+        Path goBinaryPath = goRootPath.resolve(GOBIN_RELATIVE);
+        Path archivePath = downloadArchive(version);
+        CompressUtils.decompressZipOrTarGz(archivePath.toFile(), goRootPath.toFile());
+
+        IOUtils.chmodAddX(goBinaryPath);
+
+        gorootEnv = goRootPath.toAbsolutePath().toString();
+    }
+
+    private Path downloadArchive(String version) {
+        String baseUrl = urls.get(setting.isFuckGfw());
+        String url = injectVariables(baseUrl, version);
+        String archiveFileName = injectVariables(FILENAME, version);
+        Path goBinaryCachePath = globalCacheManager.getGlobalGoBinCache(archiveFileName);
+        try {
+            httpUtils.download(url, goBinaryCachePath);
+        } catch (IOException e) {
+            throw ExceptionHandler.uncheckException(e);
+        }
+        return goBinaryCachePath;
+    }
+
+    private String injectVariables(String template, String version) {
+        String ret = template.replace("${version}", version);
+        ret = ret.replace("${os}", Os.getHostOs().toString());
+        ret = ret.replace("${arch}", Arch.getHostArch().toString());
+        ret = ret.replace("${extension}", Os.getHostOs().archiveExtension());
+        return ret;
     }
 }
--- src/test/groovy/com/github/blindpirate/gogradle/crossplatform/DefaultGoBinaryManagerTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/crossplatform/DefaultGoBinaryManagerTest.groovy
@@ -1,24 +1,154 @@
 package com.github.blindpirate.gogradle.crossplatform
 
+import com.github.blindpirate.gogradle.GogradleRunner
+import com.github.blindpirate.gogradle.GolangPluginSetting
+import com.github.blindpirate.gogradle.WithResource
+import com.github.blindpirate.gogradle.core.cache.GlobalCacheManager
+import com.github.blindpirate.gogradle.util.HttpUtils
+import com.github.blindpirate.gogradle.util.IOUtils
+import com.github.blindpirate.gogradle.util.ProcessUtils
+import com.github.blindpirate.gogradle.util.ProcessUtils.ProcessResult
+import com.github.blindpirate.gogradle.util.ProcessUtils.ProcessUtilsDelegate
+import com.github.blindpirate.gogradle.util.ReflectionUtils
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.stubbing.Answer
 
+import java.nio.file.Files
+import java.nio.file.Path
+
+import static org.mockito.Mockito.*
+
+@RunWith(GogradleRunner)
+@WithResource('')
 class DefaultGoBinaryManagerTest {
-    DefaultGoBinaryManager manager = new DefaultGoBinaryManager()
+    @Mock
+    GolangPluginSetting setting
+    @Mock
+    HttpUtils httpUtils
+    @Mock
+    GlobalCacheManager globalCacheManager
+    @Mock
+    ProcessUtilsDelegate processUtilsDelegate
+    @Mock
+    ProcessResult processResult
 
-    @Test
-    void 'smoke test'() {
-        assert manager.binaryPath() == 'go'
+    File resource
+
+    InputStream mockGoTarGz = getClass().classLoader.getResourceAsStream('mock-go-1.7.4.tar.gz')
+
+    DefaultGoBinaryManager manager
+
+    @Before
+    void setUp() {
+        ReflectionUtils.setStaticFinalField(ProcessUtils, 'DELEGATE', processUtilsDelegate)
+        Process process = mock(Process)
+        when(processUtilsDelegate.run(['go', 'version'], null, null)).thenReturn(process)
+        when(processUtilsDelegate.getResult(process)).thenReturn(processResult)
+        turnOffMockGo()
+
+        when(globalCacheManager.getGlobalGoBinCache(anyString())).thenAnswer(new Answer<Object>() {
+            @Override
+            Object answer(InvocationOnMock invocation) throws Throwable {
+                return resource.toPath().resolve(invocation.getArgument(0))
+            }
+        })
+
+        when(httpUtils.download(anyString(), any(Path))).thenAnswer(new Answer<Object>() {
+            @Override
+            Object answer(InvocationOnMock invocation) throws Throwable {
+                Path filePath = invocation.getArgument(1)
+                Files.copy(mockGoTarGz, filePath)
+                return null
+            }
+        })
+        manager = new DefaultGoBinaryManager(setting, globalCacheManager, httpUtils)
+    }
+
+    @After
+    void cleanUp() {
+        ReflectionUtils.setStaticFinalField(ProcessUtils, 'DELEGATE', new ProcessUtilsDelegate())
+    }
+
+    private turnOnMockGo() {
+        when(processResult.getStdout()).thenReturn('go version go1.7.1 darwin/amd64')
+    }
+
+    private turnOffMockGo() {
+        when(processResult.getStdout()).thenReturn('This is not a golang executable')
     }
 
     @Test
     void 'local go binary should be returned if it exists and no version specified'() {
+        // given
+        turnOnMockGo()
+        // then
+        assert manager.getBinaryPath() == 'go'
+    }
 
+    @Test
+    void 'local go binary should be returned if specified version is exactly local version'() {
+        // given
+        turnOnMockGo()
+        when(setting.getGoVersion()).thenReturn('1.7.1')
+        // then
+        assert manager.getBinaryPath() == 'go'
+    }
+
+    @Test
+    void 'the newest stable version will be used if local binary not exist and no version specified'() {
+        // given
+        when(httpUtils.get(anyString())).thenReturn('1.7.4')
+        // when
+        String binaryPath = manager.getBinaryPath()
+        // then
+        verify(httpUtils).download(anyString(), any(Path))
+        assert binaryPath == resource.toPath().resolve('1.7.4/go/bin/go').toString()
+    }
+
+    @Test
+    void 'go binary in global cache should be returned directly if it has already existed'() {
+        // given
+        when(httpUtils.get(anyString())).thenReturn('1.7.4')
+        IOUtils.write(resource, '1.7.4/go/bin/go', 'mock go binary')
+        // when
+        String binaryPath = manager.getBinaryPath()
+        // then
+        verify(httpUtils, times(0)).download(anyString(), any(Path))
+        assert binaryPath == resource.toPath().resolve('1.7.4/go/bin/go').toString()
     }
 
     @Test
     void 'go binary with specified version should be downloaded'() {
-
+        // given
+        when(setting.getGoVersion()).thenReturn("1.7.4")
+        // when
+        String binaryPath = manager.getBinaryPath()
+        // then
+        verify(httpUtils).download(anyString(), any(Path))
+        assert binaryPath == resource.toPath().resolve('1.7.4/go/bin/go').toString()
     }
 
+    @Test(expected = IllegalStateException)
+    void 'exception should be thrown when specified version is invalid'() {
+        // given
+        when(setting.getGoVersion()).thenReturn('999.999.999')
+        when(httpUtils.download(anyString(), any(Path))).thenThrow(new IOException())
+        // then
+        manager.getBinaryPath()
+    }
+
+    @Test(expected = IllegalStateException)
+    void 'exception should be thrown when network is down'() {
+        // given
+        when(httpUtils.get(anyString())).thenReturn('1.7.4')
+        when(httpUtils.download(anyString(), any(Path))).thenThrow(new IOException())
+        // then
+        manager.getBinaryPath()
+    }
 
 }
