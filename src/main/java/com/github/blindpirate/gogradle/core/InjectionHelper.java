c51b4fefc96c49f837a3844b92be46b91121a788
Bo Zhang <zhangbodut@gmail.com>
2017-01-18 18:13:01 +0800

    Add isOffline method

--- src/main/java/com/github/blindpirate/gogradle/core/InjectionHelper.java
+++ src/main/java/com/github/blindpirate/gogradle/core/InjectionHelper.java
@@ -4,6 +4,7 @@ import com.github.blindpirate.gogradle.core.dependency.GolangDependencySet;
 import com.github.blindpirate.gogradle.core.dependency.parse.MapNotationParser;
 import com.google.inject.Injector;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.gradle.api.Project;
 
 import java.util.Collection;
 import java.util.Map;
@@ -20,6 +21,11 @@ public class InjectionHelper {
             ret.add(parser.parse(notation));
         }
         return ret;
+    }
+
+    public static boolean isOffline() {
+        Project project = INJECTOR_INSTANCE.getInstance(Project.class);
+        return project.getGradle().getStartParameter().isOffline();
     }
 
 //    @SuppressFBWarnings("UWF_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD")
