7bc81a2559f7e8d3625937c2e401f3262bb7e87a
Bo Zhang <zhangbodut@gmail.com>
2017-01-18 18:12:44 +0800

    Mock project.getGradle().getStartParameters().isOffline()

--- src/integration-test/groovy/com/github/blindpirate/gogradle/support/GogradleModuleSupport.groovy
+++ src/integration-test/groovy/com/github/blindpirate/gogradle/support/GogradleModuleSupport.groovy
@@ -9,11 +9,14 @@ import org.gradle.internal.reflect.Instantiator
 import org.junit.Before
 import org.mockito.Mock
 
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS
+import static org.mockito.Mockito.mock
+
 abstract class GogradleModuleSupport {
     @Mock
     Instantiator instantiator
-    @Mock
-    ProjectInternal project
+
+    ProjectInternal project = mock(ProjectInternal, RETURNS_DEEP_STUBS)
 
     Injector injector
 
