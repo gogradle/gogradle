9a896d17b386ca24dfccab5acd6f61bffeeb60a7
Bo Zhang <zhangbodut@gmail.com>
2017-01-20 22:41:03 +0800

    Remove empty line

--- src/main/java/com/github/blindpirate/gogradle/core/dependency/tree/DependencyTreeFactory.java
+++ src/main/java/com/github/blindpirate/gogradle/core/dependency/tree/DependencyTreeFactory.java
@@ -27,7 +27,6 @@ public class DependencyTreeFactory {
 
     private DependencyTreeNode getSubTree(ResolvedDependency resolvedDependency,
                                           Set<ResolvedDependency> existedDependenciesInTree) {
-
         ResolvedDependency finalDependency = registry.retrive(resolvedDependency.getName());
 
         boolean hasExistedInTree = existedDependenciesInTree.contains(finalDependency);
