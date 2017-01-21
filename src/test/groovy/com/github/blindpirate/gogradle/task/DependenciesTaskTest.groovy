f74295604e3689c6aa608bce06b20c08c73958f2
Bo Zhang <zhangbodut@gmail.com>
2017-01-21 09:42:02 +0800

    Display both build and test dependencies in DependenciesTask

--- src/test/groovy/com/github/blindpirate/gogradle/task/DependenciesTaskTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/task/DependenciesTaskTest.groovy
@@ -2,11 +2,16 @@ package com.github.blindpirate.gogradle.task
 
 import com.github.blindpirate.gogradle.GogradleRunner
 import com.github.blindpirate.gogradle.core.dependency.tree.DependencyTreeNode
+import com.github.blindpirate.gogradle.util.ReflectionUtils
+import org.gradle.api.logging.Logger
+import org.gradle.api.logging.Logging
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 
+import static org.mockito.Mockito.verify
 import static org.mockito.Mockito.when
 
 @RunWith(GogradleRunner)
@@ -14,19 +19,37 @@ class DependenciesTaskTest extends TaskTest {
     DependenciesTask task
 
     @Mock
-    DependencyTreeNode root
+    DependencyTreeNode buildTree
+    @Mock
+    DependencyTreeNode testTree
+    @Mock
+    Logger logger
 
     @Before
     void setUp() {
         task = buildTask(DependenciesTask)
-        when(golangTaskContainer.get(ResolveTask).getDependencyTree()).thenReturn(root)
-        when(root.output()).thenReturn('output')
+        when(golangTaskContainer.get(ResolveBuildDependenciesTask).getDependencyTree()).thenReturn(buildTree)
+        when(golangTaskContainer.get(ResolveTestDependenciesTask).getDependencyTree()).thenReturn(testTree)
+        when(buildTree.output()).thenReturn('build output')
+        when(testTree.output()).thenReturn('test output')
+        ReflectionUtils.setStaticFinalField(DependenciesTask, 'LOGGER', logger)
+    }
+
+    @After
+    void cleanUp() {
+        ReflectionUtils.setStaticFinalField(DependenciesTask, 'LOGGER', Logging.getLogger(DependenciesTask))
     }
 
 
     @Test
     void 'dependency tree should be displayed correctly'() {
+        // when
         task.displayDependencies()
+        // then
+        verify(logger).quiet('build:')
+        verify(logger).quiet('test:')
+        verify(logger).quiet('build output')
+        verify(logger).quiet('test output')
     }
 
 }
