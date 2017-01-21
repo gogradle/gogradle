6fcda4670611372d3976f62e7d6d9450e812be34
Bo Zhang <zhangbodut@gmail.com>
2017-01-21 15:00:31 +0800

    Support predefined build constraints

--- src/main/java/com/github/blindpirate/gogradle/core/BuildConstraintManager.java
+++ src/main/java/com/github/blindpirate/gogradle/core/BuildConstraintManager.java
@@ -3,5 +3,9 @@ package com.github.blindpirate.gogradle.core;
 import java.util.Set;
 
 public interface BuildConstraintManager {
-    Set<String> getCtx();
+    void prepareConstraints();
+
+    Set<String> getAllConstraints();
+
+    Set<String> getExtraConstraints();
 }
--- src/test/groovy/com/github/blindpirate/gogradle/core/DefaultBuildConstraintManagerTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/core/DefaultBuildConstraintManagerTest.groovy
@@ -1,12 +1,52 @@
 package com.github.blindpirate.gogradle.core
 
+import com.github.blindpirate.gogradle.GogradleRunner
+import com.github.blindpirate.gogradle.GolangPluginSetting
+import com.github.blindpirate.gogradle.crossplatform.Arch
+import com.github.blindpirate.gogradle.crossplatform.GoBinaryManager
+import com.github.blindpirate.gogradle.crossplatform.Os
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
 
+import static org.mockito.Mockito.when
+
+@RunWith(GogradleRunner)
 class DefaultBuildConstraintManagerTest {
-    DefaultBuildConstraintManager manager = new DefaultBuildConstraintManager()
+    DefaultBuildConstraintManager manager
+
+    @Mock
+    GolangPluginSetting setting
+    @Mock
+    GoBinaryManager goBinaryManager
+
+    @Before
+    void setUp() {
+        manager = new DefaultBuildConstraintManager(goBinaryManager, setting)
+    }
 
     @Test
-    void 'getting ctx should succeed'() {
-        manager.getCtx()
+    void 'predefined constraints should be added'() {
+        // given
+        when(goBinaryManager.getGoVersion()).thenReturn("1.1")
+        // when
+        manager.prepareConstraints()
+        // then
+        assert manager.getAllConstraints().contains(Os.getHostOs().toString())
+        assert manager.getAllConstraints().contains(Arch.getHostArch().toString())
+        assert manager.getAllConstraints().contains("go1.1")
+        assert !manager.getAllConstraints().contains("go1.2")
+        assert !manager.getAllConstraints().contains("go1.0")
+    }
+
+    @Test
+    void 'all old versions should be added'() {
+        // given
+        when(goBinaryManager.getGoVersion()).thenReturn("1.100")
+        // when
+        manager.prepareConstraints()
+        // then
+        (1..100).each { assert manager.getAllConstraints().contains("go1." + it) }
     }
 }
