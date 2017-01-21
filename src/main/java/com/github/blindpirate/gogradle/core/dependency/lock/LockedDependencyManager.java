c41d72543bf24c73e4cd8fa87dbb894c274ef2c8
Bo Zhang <zhangbodut@gmail.com>
2017-01-21 10:48:15 +0800

    Add support for test dependencies

--- src/main/java/com/github/blindpirate/gogradle/core/dependency/lock/LockedDependencyManager.java
+++ src/main/java/com/github/blindpirate/gogradle/core/dependency/lock/LockedDependencyManager.java
@@ -1,12 +1,14 @@
 package com.github.blindpirate.gogradle.core.dependency.lock;
 
-import com.github.blindpirate.gogradle.core.dependency.GolangDependency;
+import com.github.blindpirate.gogradle.build.Configuration;
 import com.github.blindpirate.gogradle.core.dependency.GolangDependencySet;
+import com.github.blindpirate.gogradle.core.dependency.ResolvedDependency;
 
 import java.util.Collection;
 
 public interface LockedDependencyManager {
-    GolangDependencySet getLockedDependencies();
+    GolangDependencySet getLockedDependencies(Configuration configuration);
 
-    void lock(Collection<? extends GolangDependency> flatDependencies);
+    void lock(Collection<? extends ResolvedDependency> flatBuildDependencies,
+              Collection<? extends ResolvedDependency> flatTestDependencies);
 }
