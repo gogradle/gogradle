7f507c68395aa85d99eb3545ec666d53aed9843f
Bo Zhang <zhangbodut@gmail.com>
2017-01-22 21:31:38 +0800

    Complete TestTask

--- src/main/java/com/github/blindpirate/gogradle/task/TestTask.java
+++ src/main/java/com/github/blindpirate/gogradle/task/TestTask.java
@@ -1,6 +1,38 @@
 package com.github.blindpirate.gogradle.task;
 
-import org.gradle.api.DefaultTask;
+import com.github.blindpirate.gogradle.build.BuildManager;
+import com.github.blindpirate.gogradle.util.CollectionUtils;
+import org.gradle.api.Incubating;
+import org.gradle.api.internal.tasks.options.Option;
+import org.gradle.api.tasks.TaskAction;
 
-public class TestTask extends DefaultTask {
+import javax.inject.Inject;
+import java.util.List;
+
+public class TestTask extends AbstractGolangTask {
+
+    private List<String> testNamePattern;
+
+    @Inject
+    private BuildManager buildManager;
+
+    public TestTask() {
+        dependsOn(GolangTaskContainer.INSTALL_TEST_DEPENDENCIES_TASK_NAME);
+    }
+
+    @Option(option = "tests", description = "Sets test class or method name to be included, '*' is supported.")
+    @Incubating
+    public TestTask setTestNamePattern(List<String> testNamePattern) {
+        this.testNamePattern = testNamePattern;
+        return this;
+    }
+
+    @TaskAction
+    void test() {
+        if (CollectionUtils.isEmpty(testNamePattern)) {
+            buildManager.test();
+        } else {
+            buildManager.testWithPatterns(testNamePattern);
+        }
+    }
 }
