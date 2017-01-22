7258d40895528d84eb0f9443479b6406e330ff1f
Bo Zhang <zhangbodut@gmail.com>
2017-01-22 21:30:03 +0800

    Add a util method

--- src/main/java/com/github/blindpirate/gogradle/util/CollectionUtils.java
+++ src/main/java/com/github/blindpirate/gogradle/util/CollectionUtils.java
@@ -3,6 +3,7 @@ package com.github.blindpirate.gogradle.util;
 import org.codehaus.groovy.runtime.DefaultGroovyMethods;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +12,10 @@ import static java.util.Arrays.asList;
 import static java.util.Arrays.stream;
 
 public class CollectionUtils {
+    public static <T> boolean isEmpty(Collection<T> c) {
+        return c == null || c.isEmpty();
+    }
+
     public static <T> List<T> immutableList(T... elements) {
         return Collections.unmodifiableList(asList(elements));
     }
