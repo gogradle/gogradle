1feeda0392bada3dc49847417ee58b569db25693
Bo Zhang <zhangbodut@gmail.com>
2017-02-01 17:45:46 +0800

    Optimize import

--- src/test/groovy/com/github/blindpirate/gogradle/util/ReflectionUtils.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/util/ReflectionUtils.groovy
@@ -1,7 +1,5 @@
 package com.github.blindpirate.gogradle.util
 
-import org.gradle.api.artifacts.dsl.RepositoryHandler
-
 import java.lang.reflect.Field
 import java.lang.reflect.InvocationTargetException
 import java.lang.reflect.Method
