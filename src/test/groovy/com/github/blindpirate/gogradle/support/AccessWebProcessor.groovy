f5ebe75bf074a3922af901630d6af94e0952643c
Bo Zhang <zhangbodut@gmail.com>
2017-02-04 22:32:51 +0800

    Adjustment after refactoring

--- src/test/groovy/com/github/blindpirate/gogradle/support/AccessWebProcessor.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/support/AccessWebProcessor.groovy
@@ -2,10 +2,10 @@ package com.github.blindpirate.gogradle.support
 
 import org.junit.runners.model.FrameworkMethod
 
-class AccessWebProcessor extends GogradleRunnerProcessor {
+class AccessWebProcessor extends GogradleRunnerProcessor<AccessWeb> {
 
     @Override
-    boolean shouldIgnore(FrameworkMethod method) {
+    boolean shouldIgnore(FrameworkMethod method, AccessWeb annotation) {
         return System.getProperty('TEST_ARE_OFFLINE') != null
     }
 }
