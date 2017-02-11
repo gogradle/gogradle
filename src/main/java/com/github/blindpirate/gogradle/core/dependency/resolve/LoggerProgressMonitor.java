4bfcca20559205236bd866e2f680b60ae759c3b2
Bo Zhang <zhangbodut@gmail.com>
2017-02-11 20:39:24 +0800

    Change padding space count to 50

--- src/main/java/com/github/blindpirate/gogradle/core/dependency/resolve/LoggerProgressMonitor.java
+++ src/main/java/com/github/blindpirate/gogradle/core/dependency/resolve/LoggerProgressMonitor.java
@@ -13,7 +13,7 @@ public class LoggerProgressMonitor extends BatchingProgressMonitor {
     private static final int MAGIC_25 = 25;
     private static final int MAGIC_10 = 10;
     private static final int MAGIC_100 = 100;
-    private static final int PADDING_SPACE_COUNT = 20;
+    private static final int PADDING_SPACE_COUNT = 50;
 
 
     public LoggerProgressMonitor(String url) {
