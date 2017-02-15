cefbb84213213ac1c38956bf1e71ac47e0180688
Bo Zhang <zhangbodut@gmail.com>
2017-02-15 20:42:02 +0800

    Skeleton of MercurialAccessor

--- src/main/java/com/github/blindpirate/gogradle/vcs/mercurial/MercurialAccessor.java
+++ src/main/java/com/github/blindpirate/gogradle/vcs/mercurial/MercurialAccessor.java
@@ -1,13 +1,31 @@
 package com.github.blindpirate.gogradle.vcs.mercurial;
 
+import com.github.blindpirate.gogradle.util.ExceptionHandler;
 import com.github.blindpirate.gogradle.vcs.VcsAccessor;
+import org.tmatesoft.hg.core.HgRepositoryNotFoundException;
+import org.tmatesoft.hg.repo.HgLookup;
+import org.tmatesoft.hg.repo.HgRepository;
 
 import javax.inject.Singleton;
 import java.io.File;
+
 @Singleton
 public class MercurialAccessor implements VcsAccessor {
     @Override
     public String getRemoteUrl(File repoRoot) {
-        throw new UnsupportedOperationException("Mercurial support is under development now!");
+        HgRepository repository = getRepository(repoRoot);
+        return getRemoteUrl(repository);
+    }
+
+    public String getRemoteUrl(HgRepository repository) {
+        return repository.getConfiguration().getStringValue("paths", "default", null);
+    }
+
+    public HgRepository getRepository(File repoRoot) {
+        try {
+            return new HgLookup().detect(repoRoot);
+        } catch (HgRepositoryNotFoundException e) {
+            throw ExceptionHandler.uncheckException(e);
+        }
     }
 }
