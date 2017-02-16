1d0719ad963369ad477a5edf150065a89becc315
Bo Zhang <zhangbodut@gmail.com>
2017-02-16 21:38:10 +0800

    MercurialDependencyManager and its test

---
+++ src/main/java/com/github/blindpirate/gogradle/vcs/mercurial/MercurialDependencyManager.java
@@ -0,0 +1,148 @@
+package com.github.blindpirate.gogradle.vcs.mercurial;
+
+import com.github.blindpirate.gogradle.core.cache.GlobalCacheManager;
+import com.github.blindpirate.gogradle.core.dependency.DependencyRegistry;
+import com.github.blindpirate.gogradle.core.dependency.GolangDependencySet;
+import com.github.blindpirate.gogradle.core.dependency.NotationDependency;
+import com.github.blindpirate.gogradle.core.dependency.ResolvedDependency;
+import com.github.blindpirate.gogradle.core.dependency.VendorResolvedDependency;
+import com.github.blindpirate.gogradle.core.dependency.produce.DependencyVisitor;
+import com.github.blindpirate.gogradle.core.dependency.resolve.AbstractVcsDependencyManager;
+import com.github.blindpirate.gogradle.core.exceptions.DependencyResolutionException;
+import com.github.blindpirate.gogradle.util.Assert;
+import com.github.blindpirate.gogradle.util.IOUtils;
+import com.github.blindpirate.gogradle.util.StringUtils;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
+import org.tmatesoft.hg.core.HgChangeset;
+import org.tmatesoft.hg.repo.HgRepository;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+
+@Singleton
+public class MercurialDependencyManager extends AbstractVcsDependencyManager<HgRepository, HgChangeset> {
+    public static final String DEFAULT_BRANCH = "default";
+    private static final Logger LOGGER = Logging.getLogger(MercurialDependencyManager.class);
+
+    private final MercurialAccessor mercurialAccessor;
+
+    private final DependencyVisitor visitor;
+
+    @Inject
+    public MercurialDependencyManager(MercurialAccessor mercurialAccessor,
+                                      DependencyVisitor visitor,
+                                      GlobalCacheManager cacheManager,
+                                      DependencyRegistry dependencyRegistry) {
+        super(cacheManager, dependencyRegistry);
+        this.visitor = visitor;
+        this.mercurialAccessor = mercurialAccessor;
+    }
+
+    @Override
+    protected void doReset(ResolvedDependency dependency, Path globalCachePath) {
+        MercurialResolvedDependency resolvedDependency = (MercurialResolvedDependency) dependency;
+        HgRepository repository = mercurialAccessor.getRepository(globalCachePath.toFile());
+        mercurialAccessor.resetToSpecificNodeId(repository, resolvedDependency.getVersion());
+    }
+
+    @Override
+    protected ResolvedDependency createResolvedDependency(NotationDependency dependency,
+                                                          File directory,
+                                                          HgRepository hgRepository,
+                                                          HgChangeset hgChangeset) {
+        MercurialNotationDependency notationDependency = MercurialNotationDependency.class.cast(dependency);
+        MercurialResolvedDependency ret = MercurialResolvedDependency.builder()
+                .withNotationDependency(notationDependency)
+                .withName(dependency.getPackage().getRootPath())
+                .withNodeId(hgChangeset.getNodeid().toString())
+                .withTag(notationDependency.getTag())
+                .withCommitTime(hgChangeset.getDate().getRawTime())
+                .build();
+        GolangDependencySet dependencies = dependency.getStrategy().produce(ret, directory, visitor);
+        ret.setDependencies(dependencies);
+
+        setVendorUpdateTimeIfNecessary(hgRepository, dependencies);
+        return ret;
+    }
+
+    private void setVendorUpdateTimeIfNecessary(HgRepository repository, GolangDependencySet dependencies) {
+        dependencies.flatten().stream()
+                .filter(dependency -> dependency instanceof VendorResolvedDependency)
+                .map(dependency -> (VendorResolvedDependency) dependency)
+                .forEach(dependency -> {
+                    String relativePath = StringUtils.toUnixString(dependency.getRelativePathToHost());
+                    dependency.setUpdateTime(mercurialAccessor.getLastCommitTimeOfPath(repository, relativePath));
+                });
+    }
+
+    @Override
+    protected void resetToSpecificVersion(HgRepository hgRepository, HgChangeset hgChangeset) {
+        mercurialAccessor.resetToSpecificNodeId(hgRepository, hgChangeset.getNodeid().toString());
+    }
+
+    @Override
+    protected HgChangeset determineVersion(HgRepository repository, NotationDependency dependency) {
+        MercurialNotationDependency notationDependency = (MercurialNotationDependency) dependency;
+        if (notationDependency.getTag() != null) {
+            String tag = notationDependency.getTag();
+            Optional<HgChangeset> changeset = mercurialAccessor.findChangesetByTag(repository, tag);
+            if (changeset.isPresent()) {
+                return changeset.get();
+            }
+        }
+        if (isConcreteCommit(notationDependency.getNodeId())) {
+            String nodeId = notationDependency.getNodeId();
+            Optional<HgChangeset> changeset = mercurialAccessor.findChangesetByNodeId(repository, nodeId);
+            if (changeset.isPresent()) {
+                return changeset.get();
+            }
+        }
+
+        return mercurialAccessor.headOfBranch(repository, DEFAULT_BRANCH);
+    }
+
+    private boolean isConcreteCommit(String nodeId) {
+        return nodeId != null && !MercurialNotationDependency.NEWEST_COMMIT.equals(nodeId);
+    }
+
+    @Override
+    protected HgRepository updateRepository(NotationDependency dependency, HgRepository hgRepository, File directory) {
+        mercurialAccessor.pull(hgRepository);
+        return hgRepository;
+    }
+
+    @Override
+    protected HgRepository initRepository(NotationDependency dependency, File directory) {
+        List<String> urls = MercurialNotationDependency.class.cast(dependency).getUrls();
+        Assert.isNotEmpty(urls, "Urls cannot be empty!");
+        for (int i = 0; i < urls.size(); ++i) {
+            IOUtils.clearDirectory(directory);
+            String url = urls.get(i);
+            try {
+                return mercurialAccessor.cloneWithUrl(directory, url);
+            } catch (Throwable e) {
+                LOGGER.quiet("Cloning with url {} failed, the cause is {}", url, e.getMessage());
+                if (i == urls.size() - 1) {
+                    throw DependencyResolutionException.cannotCloneRepository(dependency, e);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected Optional<HgRepository> repositoryMatch(File directory, NotationDependency dependency) {
+        HgRepository repository = mercurialAccessor.getRepository(directory);
+        String url = mercurialAccessor.getRemoteUrl(directory);
+        if (MercurialNotationDependency.class.cast(dependency).getUrls().contains(url)) {
+            return Optional.of(repository);
+        } else {
+            return Optional.empty();
+        }
+    }
+}
---
+++ src/test/groovy/com/github/blindpirate/gogradle/vcs/mercurial/MercurialDependencyManagerTest.groovy
@@ -0,0 +1,196 @@
+package com.github.blindpirate.gogradle.vcs.mercurial
+
+import com.github.blindpirate.gogradle.GogradleRunner
+import com.github.blindpirate.gogradle.core.GolangPackage
+import com.github.blindpirate.gogradle.core.VcsGolangPackage
+import com.github.blindpirate.gogradle.core.cache.GlobalCacheManager
+import com.github.blindpirate.gogradle.core.dependency.*
+import com.github.blindpirate.gogradle.core.dependency.produce.DependencyVisitor
+import com.github.blindpirate.gogradle.core.dependency.produce.strategy.DependencyProduceStrategy
+import com.github.blindpirate.gogradle.core.exceptions.DependencyResolutionException
+import com.github.blindpirate.gogradle.support.WithResource
+import com.github.blindpirate.gogradle.vcs.VcsType
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.tmatesoft.hg.core.HgChangeset
+import org.tmatesoft.hg.core.HgDate
+import org.tmatesoft.hg.core.Nodeid
+import org.tmatesoft.hg.repo.HgRepository
+
+import java.nio.file.Paths
+import java.util.concurrent.Callable
+
+import static com.github.blindpirate.gogradle.core.dependency.resolve.AbstractVcsDependencyManagerTest.callCallableAnswer
+import static com.github.blindpirate.gogradle.util.DependencyUtils.mockWithName
+import static java.util.Optional.of
+import static org.mockito.ArgumentMatchers.any
+import static org.mockito.ArgumentMatchers.anyString
+import static org.mockito.Mockito.verify
+import static org.mockito.Mockito.when
+
+@RunWith(GogradleRunner)
+@WithResource('')
+class MercurialDependencyManagerTest {
+    @Mock
+    MercurialAccessor mercurialAccessor
+    @Mock
+    DependencyVisitor visitor
+    @Mock
+    GlobalCacheManager cacheManager
+    @Mock
+    DependencyRegistry dependencyRegistry
+    @Mock
+    HgChangeset hgChangeset
+    @Mock
+    GolangDependencySet dependencySet
+    @Mock
+    DependencyProduceStrategy strategy
+
+    MercurialNotationDependency notationDependency = mockWithName(MercurialNotationDependency, 'bitbucket.org/user/project')
+    MercurialResolvedDependency resolvedDependency = mockWithName(MercurialResolvedDependency, 'bitbucket.org/user/project')
+
+    MercurialDependencyManager manager
+
+    HgRepository repository
+
+    File resource
+
+    String repoUrl = 'https://user@bitbucket.org/user/project'
+    GolangPackage pkg = VcsGolangPackage.builder()
+            .withPath('bitbucket.org/user/project')
+            .withRootPath('bitbucket.org/user/project')
+            .withVcsType(VcsType.MERCURIAL)
+            .withUrl(repoUrl)
+            .build()
+
+    @Before
+    void setUp() {
+        manager = new MercurialDependencyManager(mercurialAccessor, visitor, cacheManager, dependencyRegistry)
+        repository = new HgRepository(resource.absolutePath)
+
+        when(cacheManager.runWithGlobalCacheLock(any(GolangDependency), any(Callable))).thenAnswer(callCallableAnswer)
+        when(cacheManager.getGlobalPackageCachePath(anyString())).thenReturn(resource.toPath())
+
+        when(mercurialAccessor.getRepository(any(File))).thenReturn(repository)
+
+        when(resolvedDependency.getVersion()).thenReturn('nodeId')
+
+        when(notationDependency.getPackage()).thenReturn(pkg)
+        when(notationDependency.getTag()).thenReturn('tag')
+        when(notationDependency.getNodeId()).thenReturn('nodeId')
+        when(notationDependency.getStrategy()).thenReturn(strategy)
+
+        when(strategy.produce(any(ResolvedDependency), any(File), any(DependencyVisitor))).thenReturn(dependencySet)
+
+        when(dependencySet.flatten()).thenReturn([])
+
+        when(hgChangeset.getNodeid()).thenReturn(Nodeid.fromAscii('1' * 40))
+        when(hgChangeset.getDate()).thenReturn(new HgDate(1L, 0))
+    }
+
+    @Test
+    void 'installing mercurial dependency should succeed'() {
+        // when
+        manager.doReset(resolvedDependency, resource.toPath())
+        // then
+        verify(mercurialAccessor).resetToSpecificNodeId(repository, 'nodeId')
+    }
+
+    @Test
+    void 'creating resolved dependency should succeed'() {
+        // given
+        VendorResolvedDependency vendorResolvedDependency = mockWithName(VendorResolvedDependency, 'vendor')
+        when(dependencySet.flatten()).thenReturn([vendorResolvedDependency])
+        when(vendorResolvedDependency.getRelativePathToHost()).thenReturn(Paths.get('relative/path'))
+        // when
+        ResolvedDependency result = manager.createResolvedDependency(notationDependency, resource, repository, hgChangeset)
+        // then
+        assert result instanceof MercurialResolvedDependency
+        assert result.tag == 'tag'
+        assert result.name == 'bitbucket.org/user/project'
+        assert result.version == '1' * 40
+        assert result.updateTime == 1L
+        verify(mercurialAccessor).getLastCommitTimeOfPath(repository, 'relative/path')
+    }
+
+    @Test
+    void 'tag should be used if it exists'() {
+        // given
+        when(mercurialAccessor.findChangesetByTag(repository, 'tag')).thenReturn(of(hgChangeset))
+        // then
+        assert manager.determineVersion(repository, notationDependency) == hgChangeset
+    }
+
+    @Test
+    void 'nodeId should be used if it exists'() {
+        // given
+        when(mercurialAccessor.findChangesetByNodeId(repository, 'nodeId')).thenReturn(of(hgChangeset))
+        // then
+        assert manager.determineVersion(repository, notationDependency) == hgChangeset
+    }
+
+    @Test
+    void 'head commit should be used if tag and nodeId do not exist'() {
+        // given
+        when(mercurialAccessor.headOfBranch(repository, 'default')).thenReturn(hgChangeset)
+        // then
+        assert manager.determineVersion(repository, notationDependency) == hgChangeset
+    }
+
+    @Test
+    void 'head commit should be used if nodeId is NEWEST_COMMIT'() {
+        // given
+        when(notationDependency.getNodeId()).thenReturn('NEWEST_COMMIT')
+        when(mercurialAccessor.headOfBranch(repository, 'default')).thenReturn(hgChangeset)
+        // then
+        assert manager.determineVersion(repository, notationDependency) == hgChangeset
+    }
+
+    @Test
+    void 'repository should be updated successfully'() {
+        // when
+        manager.updateRepository(notationDependency, repository, resource)
+        // then
+        verify(mercurialAccessor).pull(repository)
+    }
+
+    @Test
+    void 'all urls should be tried to clone repo'() {
+        // given
+        when(notationDependency.getUrls()).thenReturn(['url1', 'url2'])
+        when(mercurialAccessor.cloneWithUrl(resource, 'url1')).thenThrow(new IllegalStateException())
+        // when
+        manager.initRepository(notationDependency, resource)
+        // then
+        verify(mercurialAccessor).cloneWithUrl(resource, 'url2')
+    }
+
+    @Test(expected = DependencyResolutionException)
+    void 'exception should be thrown if all urls are tried'() {
+        // when
+        when(notationDependency.getUrls()).thenReturn(['url1', 'url2'])
+        when(mercurialAccessor.cloneWithUrl(any(File), anyString())).thenThrow(new IllegalStateException())
+        // then
+        manager.initRepository(notationDependency, resource)
+    }
+
+    @Test
+    void 'repo should be considered matched if url matched'() {
+        // given
+        when(mercurialAccessor.getRemoteUrl(resource)).thenReturn('url')
+        when(notationDependency.getUrls()).thenReturn(['url', 'url2'])
+        // then
+        assert manager.repositoryMatch(resource, notationDependency).isPresent()
+    }
+
+    @Test
+    void 'repo should be considered unmatched if url not matched'() {
+        // given
+        when(mercurialAccessor.getRemoteUrl(repository)).thenReturn('url')
+        when(notationDependency.getUrls()).thenReturn(['url1', 'url2'])
+        // then
+        assert !manager.repositoryMatch(resource, notationDependency).isPresent()
+    }
+}
