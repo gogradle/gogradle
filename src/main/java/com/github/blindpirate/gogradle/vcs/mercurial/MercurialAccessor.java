5be104d7c34cf43ccd88600382fa8fdc52000af4
Bo Zhang <zhangbodut@gmail.com>
2017-02-17 09:23:57 +0800

    MercurialAccessor and tests

--- src/main/java/com/github/blindpirate/gogradle/vcs/mercurial/MercurialAccessor.java
+++ src/main/java/com/github/blindpirate/gogradle/vcs/mercurial/MercurialAccessor.java
@@ -2,15 +2,34 @@ package com.github.blindpirate.gogradle.vcs.mercurial;
 
 import com.github.blindpirate.gogradle.util.ExceptionHandler;
 import com.github.blindpirate.gogradle.vcs.VcsAccessor;
+import org.tmatesoft.hg.core.HgChangeset;
+import org.tmatesoft.hg.core.HgCheckoutCommand;
+import org.tmatesoft.hg.core.HgCloneCommand;
+import org.tmatesoft.hg.core.HgException;
+import org.tmatesoft.hg.core.HgIOException;
+import org.tmatesoft.hg.core.HgLibraryFailureException;
+import org.tmatesoft.hg.core.HgLogCommand;
+import org.tmatesoft.hg.core.HgPullCommand;
+import org.tmatesoft.hg.core.HgRemoteConnectionException;
 import org.tmatesoft.hg.core.HgRepositoryNotFoundException;
+import org.tmatesoft.hg.core.Nodeid;
 import org.tmatesoft.hg.repo.HgLookup;
+import org.tmatesoft.hg.repo.HgRemoteRepository;
 import org.tmatesoft.hg.repo.HgRepository;
+import org.tmatesoft.hg.repo.HgTags;
+import org.tmatesoft.hg.util.CancelledException;
 
 import javax.inject.Singleton;
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
 
 @Singleton
 public class MercurialAccessor implements VcsAccessor {
+    private HgLookup hgLookup = new HgLookup();
+
     @Override
     public String getRemoteUrl(File repoRoot) {
         HgRepository repository = getRepository(repoRoot);
@@ -28,4 +47,82 @@ public class MercurialAccessor implements VcsAccessor {
             throw ExceptionHandler.uncheckException(e);
         }
     }
+
+    public long getLastCommitTimeOfPath(HgRepository repository, String relativePath) {
+        try {
+            List<HgChangeset> changesets = new HgLogCommand(repository).file(relativePath, false).execute();
+            return changesets.get(0).getDate().getRawTime();
+        } catch (HgException e) {
+            throw ExceptionHandler.uncheckException(e);
+        }
+    }
+
+    public Optional<HgChangeset> findChangesetByTag(HgRepository repository, String tag) {
+        HgTags tags = repository.getTags();
+        HgTags.TagInfo tagInfo = tags.getAllTags().get(tag);
+        if (tagInfo == null) {
+            return Optional.empty();
+        } else {
+            return findChangesetByNodeId(repository, tagInfo.revision());
+        }
+    }
+
+    public Optional<HgChangeset> findChangesetByNodeId(HgRepository repository, String nodeId) {
+        return findChangesetByNodeId(repository, Nodeid.fromAscii(nodeId));
+    }
+
+    public Optional<HgChangeset> findChangesetByNodeId(HgRepository repository, Nodeid nodeId) {
+        HgLogCommand logCommand = new HgLogCommand(repository);
+        try {
+            List<HgChangeset> changesets = logCommand.changeset(nodeId).execute();
+            if (changesets.isEmpty()) {
+                return Optional.empty();
+            } else {
+                return Optional.of(changesets.get(0));
+            }
+        } catch (HgException e) {
+            throw ExceptionHandler.uncheckException(e);
+        }
+
+    }
+
+    public HgChangeset headOfBranch(HgRepository repository, String defaultBranch) {
+        HgLogCommand logCommand = new HgLogCommand(repository).branch(defaultBranch);
+        try {
+            List<HgChangeset> changesets = logCommand.execute();
+            return changesets.get(changesets.size() - 1);
+        } catch (HgException e) {
+            throw ExceptionHandler.uncheckException(e);
+        }
+    }
+
+    public void pull(HgRepository repository) {
+        HgPullCommand cmd = new HgPullCommand(repository);
+        try {
+            cmd.execute();
+        } catch (HgRemoteConnectionException | HgIOException | CancelledException | HgLibraryFailureException e) {
+            throw ExceptionHandler.uncheckException(e);
+        }
+    }
+
+    public HgRepository cloneWithUrl(File dir, String url) {
+        try {
+            HgRemoteRepository remoteRepository = hgLookup.detectRemote(new URI(url));
+            return new HgCloneCommand().source(remoteRepository).destination(dir).execute();
+        } catch (URISyntaxException | CancelledException | HgException e) {
+            throw ExceptionHandler.uncheckException(e);
+        }
+    }
+
+    public void resetToSpecificNodeId(HgRepository repository, String nodeId) {
+        HgCheckoutCommand cmd = new HgCheckoutCommand(repository);
+        try {
+            cmd.changeset(Nodeid.fromAscii(nodeId));
+            cmd.execute();
+        } catch (CancelledException | HgException e) {
+            throw ExceptionHandler.uncheckException(e);
+        }
+    }
+
+
 }
---
+++ src/test/groovy/com/github/blindpirate/gogradle/vcs/mercurial/MercurialAccessorTest.groovy
@@ -0,0 +1,67 @@
+package com.github.blindpirate.gogradle.vcs.mercurial
+
+import com.github.blindpirate.gogradle.GogradleRunner
+import com.github.blindpirate.gogradle.support.AccessWeb
+import com.github.blindpirate.gogradle.support.WithResource
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.tmatesoft.hg.repo.HgRepository
+
+@RunWith(GogradleRunner)
+@WithResource('test-for-gogradle-hg.zip')
+class MercurialAccessorTest {
+
+    MercurialAccessor accessor = new MercurialAccessor()
+
+    File resource
+
+    HgRepository repository
+
+    @Test
+    void 'getting remote url should succeed'() {
+        assert accessor.getRemoteUrl(resource) == 'https://blindpirate@bitbucket.org/blindpirate/test-for-gogradle'
+    }
+
+    @Test
+    void 'getting latest commit should succeed'() {
+        repository = accessor.getRepository(resource)
+        // 2017/2/16 21:45:31 UTC+8
+        assert accessor.getLastCommitTimeOfPath(repository, 'commit1') == 1487252731000
+    }
+
+    @Test
+    void 'finding changeset by tag should succeed'() {
+        repository = accessor.getRepository(resource)
+        assert accessor.findChangesetByTag(repository, 'commit2_tag').get().nodeid.toString() == '1eaebd519f4c3f7d793b9ff42328d4383d672529'
+    }
+
+    @Test
+    void 'getting head of branch should succeed'() {
+        repository = accessor.getRepository(resource)
+        assert accessor.headOfBranch(repository, 'default').nodeid.toString() == '620889544e2db8b064180431bcd1bb965704f4c2'
+    }
+
+    @Test
+    @AccessWeb
+    void 'pulling should succeed'() {
+        repository = accessor.getRepository(resource)
+        accessor.pull(repository)
+        assert new File(resource, 'commit3').exists()
+    }
+
+    @Test
+    @AccessWeb
+    @WithResource('')
+    void 'cloning should succeed'() {
+        accessor.cloneWithUrl(resource, 'https://blindpirate@bitbucket.org/blindpirate/test-for-gogradle')
+//        accessor.cloneWithUrl(resource, 'ssh://hg@bitbucket.org/blindpirate/test-for-gogradle')
+        assert new File(resource, 'commit1').exists()
+    }
+
+    @Test
+    void 'resetting should succeed'() {
+        repository = accessor.getRepository(resource)
+        accessor.resetToSpecificNodeId(repository, '5aa10')
+        assert !new File(resource, 'commit2').exists()
+    }
+}
