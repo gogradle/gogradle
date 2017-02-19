deb6990e576ac0b6a3982ff196d76020aff8d4eb
Bo Zhang <zhangbodut@gmail.com>
2017-02-19 22:17:11 +0800

    Support test for BuildAction

--- src/integration-test/groovy/com/github/blindpirate/gogradle/support/IntegrationTestSupport.groovy
+++ src/integration-test/groovy/com/github/blindpirate/gogradle/support/IntegrationTestSupport.groovy
@@ -3,6 +3,9 @@ package com.github.blindpirate.gogradle.support
 import com.github.blindpirate.gogradle.GolangPlugin
 import com.github.blindpirate.gogradle.crossplatform.Os
 import com.github.blindpirate.gogradle.util.IOUtils
+import org.gradle.tooling.BuildAction
+import org.gradle.tooling.BuildActionExecuter
+import org.gradle.tooling.BuildController
 import org.gradle.tooling.BuildLauncher
 import org.gradle.tooling.ConfigurableLauncher
 import org.gradle.tooling.GradleConnector
@@ -65,6 +68,21 @@ golang {
             closure(build)
 
             build.run()
+        } finally {
+            connection.close()
+        }
+    }
+
+    Object buildAction(Closure closure) {
+        ProjectConnection connection = newProjectConnection()
+        try {
+            BuildActionExecuter executor = connection.action(new BuildAction() {
+                @Override
+                Object execute(BuildController controller) {
+                    closure(controller)
+                }
+            })
+            return executor.run()
         } finally {
             connection.close()
         }
