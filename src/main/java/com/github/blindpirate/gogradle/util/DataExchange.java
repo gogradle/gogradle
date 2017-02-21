9fff3cc0e0b769375cc5ce94a42b2350722870cc
Bo Zhang <zhangbodut@gmail.com>
2017-02-21 20:26:49 +0800

    Add support for xml

--- src/main/java/com/github/blindpirate/gogradle/util/DataExchange.java
+++ src/main/java/com/github/blindpirate/gogradle/util/DataExchange.java
@@ -1,17 +1,28 @@
 package com.github.blindpirate.gogradle.util;
 
+import com.ctc.wstx.stax.WstxInputFactory;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.File;
 import java.io.IOException;
 
+import static com.ctc.wstx.api.WstxInputProperties.PARSING_MODE_DOCUMENTS;
 import static com.github.blindpirate.gogradle.util.ExceptionHandler.uncheckException;
 
 public class DataExchange {
     private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
     private static final ObjectMapper YAML_MAPPER = new ObjectMapper(new YAMLFactory());
+    private static XmlMapper XML_MAPPER;
+
+    static {
+        // Override the default config, otherwise it will not ignore <?xml ...>
+        WstxInputFactory inputFactory = new WstxInputFactory();
+        inputFactory.getConfig().setInputParsingMode(PARSING_MODE_DOCUMENTS);
+        XML_MAPPER = new XmlMapper(inputFactory);
+    }
 
     public static <T> T parseJson(File file, Class<T> clazz) {
         return parseWithMapper(JSON_MAPPER, file, clazz);
@@ -23,6 +34,10 @@ public class DataExchange {
 
     public static <T> T parseYaml(File file, Class<T> clazz) {
         return parseWithMapper(YAML_MAPPER, file, clazz);
+    }
+
+    public static <T> T parseXml(File file, Class<T> clazz) {
+        return parseWithMapper(XML_MAPPER, file, clazz);
     }
 
     private static <T> T parseWithMapper(ObjectMapper mapper, String s, Class<T> clazz) {
