ee5a4d519ac7b232fe9bd6de17ee5eda8de72c27
Bo Zhang <zhangbodut@gmail.com>
2017-02-25 21:27:52 +0800

    Test for generating gogland xmls

---
+++ src/main/java/com/github/blindpirate/gogradle/ide/GoglandIntegration.java
@@ -0,0 +1,58 @@
+package com.github.blindpirate.gogradle.ide;
+
+import com.github.blindpirate.gogradle.crossplatform.GoBinaryManager;
+import com.google.inject.Inject;
+import org.gradle.api.Project;
+
+import javax.inject.Singleton;
+
+@Singleton
+@SuppressWarnings("checkstyle:linelength")
+public class GoglandIntegration extends IdeIntegration {
+    private static final String MODULE_IML_PATH = ".idea/${projectName}.iml";
+    private static final String MODULE_IML_CONTENT = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<module type=\"${moduleType}\" version=\"4\">\n"
+            + "  <component name=\"NewModuleRootManager\">\n"
+            + "    <content url=\"file://$MODULE_DIR$\" />\n"
+            + "    <orderEntry type=\"sourceFolder\" forTests=\"false\" />\n"
+            + "    <orderEntry type=\"library\" scope=\"PROVIDED\" name=\"Go_${goVersionUnderscore}\" level=\"project\" />\n"
+            + "  </component>\n"
+            + "</module>";
+
+    private static final String GO_SDK_DOT_XML_PATH = ".idea/libraries/Go_${goVersionUnderscore}.xml";
+    private static final String GO_SDK_DOT_XML_CONTENT = "<component name=\"libraryTable\">\n"
+            + "  <library name=\"Go_${goVersionUnderscore}\">\n"
+            + "    <CLASSES>\n"
+            + "      <root url=\"file://${goRootSrc}\" />\n"
+            + "    </CLASSES>\n"
+            + "    <SOURCES>\n"
+            + "      <root url=\"file://${goRootSrc}\" />\n"
+            + "    </SOURCES>\n"
+            + "  </library>\n"
+            + "</component>";
+
+    @Inject
+    public GoglandIntegration(GoBinaryManager goBinaryManager, Project project) {
+        super(goBinaryManager, project);
+    }
+
+    @Override
+    protected String getModuleType() {
+        return "WEB_MODULE";
+    }
+
+    @Override
+    protected String getModuleImlDir() {
+        return ".idea";
+    }
+
+    @Override
+    protected void generateModuleIml() {
+        writeFileIntoProjectRoot(render(MODULE_IML_PATH), render(MODULE_IML_CONTENT));
+    }
+
+    @Override
+    protected void generateGoSdkDotXml() {
+        writeFileIntoProjectRoot(render(GO_SDK_DOT_XML_PATH), render(GO_SDK_DOT_XML_CONTENT));
+    }
+}
---
+++ src/test/groovy/com/github/blindpirate/gogradle/ide/GoglandIntegrationTest.groovy
@@ -0,0 +1,53 @@
+package com.github.blindpirate.gogradle.ide
+
+import com.github.blindpirate.gogradle.GogradleRunner
+import com.github.blindpirate.gogradle.crossplatform.GoBinaryManager
+import com.github.blindpirate.gogradle.support.WithResource
+import com.github.blindpirate.gogradle.util.IOUtils
+import org.gradle.api.Project
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+
+import static org.mockito.Mockito.when
+
+@RunWith(GogradleRunner)
+@WithResource('')
+class GoglandIntegrationTest {
+    @Mock
+    GoBinaryManager manager
+    @Mock
+    Project project
+    File resource
+
+    GoglandIntegration goglandIntegration
+
+    @Before
+    void setUp() {
+        goglandIntegration = new GoglandIntegration(manager, project)
+        when(project.getRootDir()).thenReturn(resource)
+        when(manager.getBinaryPath()).thenReturn(new File(resource, 'go/bin/go').toPath())
+        when(manager.getGoroot()).thenReturn(new File(resource, 'go').toPath())
+        when(manager.getGoVersion()).thenReturn('1.7.1')
+
+        when(project.getName()).thenReturn('MyAwesomeProject')
+    }
+
+    @Test
+    void 'gogland xmls should be generated correctly'() {
+        goglandIntegration.generateIdeaXmls()
+
+        assert new File(resource, '.idea/goLibraries.xml').exists()
+
+        String moduleIml = IOUtils.toString(new File(resource, '.idea/MyAwesomeProject.iml'))
+        assert moduleIml.contains('WEB_MODULE')
+        assert moduleIml.contains('Go_1_7_1')
+
+        String goSdkXml = IOUtils.toString(new File(resource, '.idea/libraries/Go_1_7_1.xml'))
+        assert goSdkXml.contains("file://${new File(resource, 'go/src').getAbsolutePath()}")
+
+        String modulesXml = IOUtils.toString(new File(resource, '.idea/modules.xml'))
+        assert modulesXml.contains('.idea/MyAwesomeProject.iml')
+    }
+}
