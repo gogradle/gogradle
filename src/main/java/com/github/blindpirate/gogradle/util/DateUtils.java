13e46d325daa140ac79a8a760dbf35b46c4ee5cf
Bo Zhang <zhangbodut@gmail.com>
2017-02-25 20:04:51 +0800

    Add a utility method for DateUtils and complete tests

--- src/main/java/com/github/blindpirate/gogradle/util/DateUtils.java
+++ src/main/java/com/github/blindpirate/gogradle/util/DateUtils.java
@@ -1,7 +1,14 @@
 package com.github.blindpirate.gogradle.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateUtils {
     public static long toMilliseconds(long second) {
         return 1000L * second;
+    }
+
+    public static String formatNow(String pattern) {
+        return new SimpleDateFormat(pattern).format(new Date());
     }
 }
