b81559b5a24a916d6c0ade250f1c51ae6061e114
Bo Zhang <zhangbodut@gmail.com>
2017-03-12 07:38:05 +0800

    Refactor BuildTask and its test

--- src/main/java/com/github/blindpirate/gogradle/task/BuildTask.java
+++ src/main/java/com/github/blindpirate/gogradle/task/BuildTask.java
@@ -1,21 +1,57 @@
 package com.github.blindpirate.gogradle.task;
 
-import com.github.blindpirate.gogradle.build.BuildManager;
-import org.gradle.api.tasks.TaskAction;
+import com.github.blindpirate.gogradle.GolangPluginSetting;
+import com.github.blindpirate.gogradle.crossplatform.Arch;
+import com.github.blindpirate.gogradle.crossplatform.Os;
+import com.github.blindpirate.gogradle.util.MapUtils;
+import groovy.lang.Closure;
+import org.apache.commons.lang3.tuple.Pair;
+import org.gradle.api.Task;
 
 import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class BuildTask extends AbstractGolangTask {
+public class BuildTask extends Go {
 
     @Inject
-    private BuildManager buildManager;
+    private GolangPluginSetting setting;
 
     public BuildTask() {
         dependsOn(GolangTaskContainer.INSTALL_BUILD_DEPENDENCIES_TASK_NAME);
     }
 
-    @TaskAction
-    public void build() {
-        buildManager.build();
+    @Override
+    protected void doAddDefaultAction() {
+        getEnvs().forEach(this::addOneBuildAction);
+    }
+
+    private void addOneBuildAction(Map<String, String> env) {
+        List<String> args = Arrays.asList("build", "-o", "./gogradle/${GOOS}_${GOARCH}_${PROJECT_NAME}");
+        doLast(task -> buildManager.go(args, env));
+    }
+
+
+    @Override
+    public Task doLast(final Closure closure) {
+        List<Map<String, String>> envs = getEnvs();
+        envs.forEach(env -> doLast(GoExecutionAction.wrapClousureWithEnvs(closure, env)));
+        return this;
+    }
+
+    private List<Map<String, String>> getEnvs() {
+        List<Map<String, String>> ret = setting.getTargetPlatforms().stream()
+                .map(this::pairToEnv)
+                .collect(Collectors.toList());
+        ret.forEach(env -> env.put("GOPATH", buildManager.getBuildGopath()));
+        return ret;
+    }
+
+    private Map<String, String> pairToEnv(Pair<Os, Arch> osAndArches) {
+        return MapUtils.asMap("GOOS", osAndArches.getLeft().toString(),
+                "GOARCH", osAndArches.getRight().toString(),
+                "GOEXE", osAndArches.getLeft().exeExtension());
     }
 }
--- src/test/groovy/com/github/blindpirate/gogradle/task/BuildTaskTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/task/BuildTaskTest.groovy
@@ -1,6 +1,9 @@
 package com.github.blindpirate.gogradle.task
 
 import com.github.blindpirate.gogradle.GogradleRunner
+import com.github.blindpirate.gogradle.crossplatform.Arch
+import com.github.blindpirate.gogradle.crossplatform.Os
+import org.apache.commons.lang3.tuple.Pair
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,14 +18,41 @@ class BuildTaskTest extends TaskTest {
     @Before
     void setUp() {
         task = buildTask(BuildTask)
+        when(setting.getTargetPlatforms()).thenReturn(
+                [Pair.of(Os.DARWIN, Arch.AMD64),
+                 Pair.of(Os.LINUX, Arch.I386),
+                 Pair.of(Os.WINDOWS, Arch.AMD64)])
+        when(buildManager.getBuildGopath()).thenReturn('build_gopath')
     }
 
     @Test
-    void 'build task should be executed successfully'() {
+    void 'adding default action should succeed'() {
         // when
-        task.build()
+        task.addDefaultActionIfNoCustomActions()
+        task.actions.each { it.execute(task) }
         // then
-        verify(buildManager).build()
-        assertTaskDependsOn(task, INSTALL_BUILD_DEPENDENCIES_TASK_NAME)
+        assert task.actions.size() == 3
+        verify(buildManager).go(['build', '-o', './gogradle/${GOOS}_${GOARCH}_${PROJECT_NAME}'], [GOOS: 'darwin', GOARCH: 'amd64', GOEXE: '', GOPATH: 'build_gopath'])
+        verify(buildManager).go(['build', '-o', './gogradle/${GOOS}_${GOARCH}_${PROJECT_NAME}'], [GOOS: 'linux', GOARCH: '386', GOEXE: '', GOPATH: 'build_gopath'])
+        verify(buildManager).go(['build', '-o', './gogradle/${GOOS}_${GOARCH}_${PROJECT_NAME}'], [GOOS: 'windows', GOARCH: 'amd64', GOEXE: '.exe', GOPATH: 'build_gopath'])
+    }
+
+    @Test
+    void 'default action should not be added if there is customized action'() {
+        // when
+        when(setting.getTargetPlatforms()).thenReturn([Pair.of(Os.hostOs, Arch.hostArch)])
+        task.doLast {}
+        task.addDefaultActionIfNoCustomActions()
+        // then
+        assert task.actions.size() == 1
+    }
+
+    @Test
+    void 'custom action should be expanded when added'() {
+        // when
+        task.doLast {}
+        // then
+        assert task.actions.size() == 3
+        assert task.actions.any { it instanceof GoExecutionAction }
     }
 }
