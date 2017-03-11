0d7de85cf161f1e31cdf74a3f4303641deff2957
Bo Zhang <zhangbodut@gmail.com>
2017-03-12 07:55:35 +0800

    Refactor TestTask and its test

--- src/main/java/com/github/blindpirate/gogradle/task/TestTask.java
+++ src/main/java/com/github/blindpirate/gogradle/task/TestTask.java
@@ -1,23 +1,36 @@
 package com.github.blindpirate.gogradle.task;
 
-import com.github.blindpirate.gogradle.build.BuildManager;
-import com.github.blindpirate.gogradle.util.CollectionUtils;
+import com.github.blindpirate.gogradle.util.IOUtils;
+import com.github.blindpirate.gogradle.util.StringUtils;
 import org.gradle.api.Incubating;
+import org.gradle.api.Project;
 import org.gradle.api.internal.tasks.options.Option;
-import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
 
 import javax.inject.Inject;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.github.blindpirate.gogradle.task.GolangTaskContainer.INSTALL_BUILD_DEPENDENCIES_TASK_NAME;
 import static com.github.blindpirate.gogradle.task.GolangTaskContainer.INSTALL_TEST_DEPENDENCIES_TASK_NAME;
+import static com.github.blindpirate.gogradle.util.CollectionUtils.asStringList;
+import static com.github.blindpirate.gogradle.util.CollectionUtils.isEmpty;
+import static com.github.blindpirate.gogradle.util.IOUtils.filterTestsMatchingPatterns;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class TestTask extends AbstractGolangTask {
-
-    private List<String> testNamePattern;
+public class TestTask extends Go {
+    private static final Logger LOGGER = Logging.getLogger(TestTask.class);
 
     @Inject
-    private BuildManager buildManager;
+    private Project project;
+
+    private List<String> testNamePattern;
 
     public TestTask() {
         dependsOn(INSTALL_BUILD_DEPENDENCIES_TASK_NAME,
@@ -31,12 +44,47 @@ public class TestTask extends AbstractGolangTask {
         return this;
     }
 
-    @TaskAction
-    void test() {
-        if (CollectionUtils.isEmpty(testNamePattern)) {
-            buildManager.test();
+    @Override
+    protected void doAddDefaultAction() {
+        if (isEmpty(testNamePattern)) {
+            addTestAllAction();
         } else {
-            buildManager.testWithPatterns(testNamePattern);
+            addTestActions();
         }
+    }
+
+    private void addTestActions() {
+        Collection<File> filesMatchingPatterns = filterTestsMatchingPatterns(project.getRootDir(), testNamePattern);
+        if (filesMatchingPatterns.isEmpty()) {
+            LOGGER.quiet("No tests matching " + testNamePattern.stream().collect(joining("/")) + ", skip.");
+        } else {
+            LOGGER.quiet("Found " + filesMatchingPatterns.size() + " tests to run.");
+
+            Map<File, List<File>> groupByParentDir = filesMatchingPatterns.stream()
+                    .collect(Collectors.groupingBy(File::getParentFile));
+
+            groupByParentDir.forEach((parentDir, tests) -> {
+                List<String> fullPaths = tests.stream()
+                        .map(File::getAbsolutePath).collect(toList());
+                fullPaths.addAll(getAllNonTestGoFiles(parentDir));
+                doLast(task -> buildManager.go(asStringList("test", fullPaths), null));
+            });
+        }
+    }
+
+    private void addTestAllAction() {
+        // https://golang.org/cmd/go/#hdr-Description_of_package_lists
+        doLast(task -> buildManager.go(Arrays.asList("test", "./..."), null));
+    }
+
+    private List<String> getAllNonTestGoFiles(File dir) {
+        List<String> names = IOUtils.safeList(dir);
+        return names.stream()
+                .filter(name -> name.endsWith(".go"))
+                .filter(name -> !StringUtils.startsWithAny(name, "_", "."))
+                .filter(name -> !name.endsWith("_test.go"))
+                .map(name -> new File(dir, name))
+                .map(File::getAbsolutePath)
+                .collect(toList());
     }
 }
--- src/test/groovy/com/github/blindpirate/gogradle/task/TestTaskTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/task/TestTaskTest.groovy
@@ -1,6 +1,8 @@
 package com.github.blindpirate.gogradle.task
 
 import com.github.blindpirate.gogradle.GogradleRunner
+import com.github.blindpirate.gogradle.support.WithResource
+import com.github.blindpirate.gogradle.util.IOUtils
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -8,14 +10,19 @@ import org.junit.runner.RunWith
 import static com.github.blindpirate.gogradle.task.GolangTaskContainer.INSTALL_BUILD_DEPENDENCIES_TASK_NAME
 import static com.github.blindpirate.gogradle.task.GolangTaskContainer.INSTALL_TEST_DEPENDENCIES_TASK_NAME
 import static org.mockito.Mockito.verify
+import static org.mockito.Mockito.when
 
 @RunWith(GogradleRunner)
+@WithResource('')
 class TestTaskTest extends TaskTest {
     TestTask task
+
+    File resource
 
     @Before
     void setUp() {
         task = buildTask(TestTask)
+        when(project.getRootDir()).thenReturn(resource)
     }
 
     @Test
@@ -25,20 +32,28 @@ class TestTaskTest extends TaskTest {
     }
 
     @Test
-    void 'test task should be executed properly'() {
+    void 'all package should be tested if not specified'() {
         // when
-        task.test()
+        task.addDefaultActionIfNoCustomActions()
+        task.actions[0].execute(task)
         // then
-        verify(buildManager).test()
+        verify(buildManager).go(['test', './...'], null)
     }
 
     @Test
-    void 'test task should be executed with pattern when provided'() {
+    void 'test with specific pattern should succeed'() {
         // given
-        task.setTestNamePattern(['pattern'])
+        File a1 = IOUtils.write(resource, 'a/a1_test.go', '')
+        File a2 = IOUtils.write(resource, 'a/a2_test.go', '')
+        File a3 = IOUtils.write(resource, 'a/a3.go', '')
+        File b1 = IOUtils.write(resource, 'b/b1_test.go', '')
+        File b2 = IOUtils.write(resource, 'b/b2.go', '')
         // when
-        task.test()
+        task.setTestNamePattern(['*_test*'])
+        task.addDefaultActionIfNoCustomActions()
+        task.actions.each {it.execute(task)}
         // then
-        verify(buildManager).testWithPatterns(['pattern'])
+        verify(buildManager).go(['test', b1.absolutePath, b2.absolutePath], null)
+        verify(buildManager).go(['test', a1.absolutePath, a2.absolutePath, a3.absolutePath], null)
     }
 }
