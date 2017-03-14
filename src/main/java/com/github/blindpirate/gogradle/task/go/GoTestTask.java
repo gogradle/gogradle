4448dea2368b59b7554ba98902f41a0e04442cfa
Bo Zhang <zhangbodut@gmail.com>
2017-03-14 19:33:54 +0800

    Refactor test task and its test

--- src/main/java/com/github/blindpirate/gogradle/task/go/GoTestTask.java
+++ src/main/java/com/github/blindpirate/gogradle/task/go/GoTestTask.java
@@ -1,28 +1,41 @@
 package com.github.blindpirate.gogradle.task.go;
 
+import com.github.blindpirate.gogradle.GolangPluginSetting;
 import com.github.blindpirate.gogradle.build.TestPatternFilter;
+import com.github.blindpirate.gogradle.util.CollectionUtils;
 import com.github.blindpirate.gogradle.util.IOUtils;
-import com.github.blindpirate.gogradle.util.StringUtils;
+import org.gradle.api.Action;
 import org.gradle.api.Incubating;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
 import org.gradle.api.internal.tasks.options.Option;
+import org.gradle.api.internal.tasks.testing.junit.report.DefaultTestReport;
+import org.gradle.api.internal.tasks.testing.junit.result.TestClassResult;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
+import org.gradle.internal.operations.BuildOperationProcessor;
 
 import javax.inject.Inject;
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.function.Consumer;
 
 import static com.github.blindpirate.gogradle.common.GoSourceCodeFilter.TEST_GO_FILTER;
 import static com.github.blindpirate.gogradle.task.GolangTaskContainer.INSTALL_BUILD_DEPENDENCIES_TASK_NAME;
 import static com.github.blindpirate.gogradle.task.GolangTaskContainer.INSTALL_TEST_DEPENDENCIES_TASK_NAME;
-import static com.github.blindpirate.gogradle.util.CollectionUtils.asStringList;
 import static com.github.blindpirate.gogradle.util.CollectionUtils.isEmpty;
 import static com.github.blindpirate.gogradle.util.IOUtils.filterFilesRecursively;
-import static com.github.blindpirate.gogradle.util.IOUtils.safeList;
+import static com.github.blindpirate.gogradle.util.IOUtils.safeListFiles;
+import static com.github.blindpirate.gogradle.util.StringUtils.fileNameEndsWithAny;
+import static com.github.blindpirate.gogradle.util.StringUtils.fileNameStartsWithAny;
+import static com.github.blindpirate.gogradle.util.StringUtils.toUnixString;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
@@ -31,6 +44,15 @@ public class GoTestTask extends Go {
 
     @Inject
     private Project project;
+
+    @Inject
+    private GolangPluginSetting setting;
+
+    @Inject
+    private BuildOperationProcessor buildOperationProcessor;
+
+    @Inject
+    private GoTestStdoutExtractor extractor;
 
     private List<String> testNamePattern;
 
@@ -66,11 +88,10 @@ public class GoTestTask extends Go {
             Map<File, List<File>> parentDirToFiles = groupByParentDir(filesMatchingPatterns);
 
             parentDirToFiles.forEach((parentDir, tests) -> {
-                List<String> fullPaths = tests.stream()
-                        .map(File::getAbsolutePath).collect(toList());
-                fullPaths.addAll(getAllNonTestGoFiles(parentDir));
-                doLast(task -> buildManager.go(asStringList("test", fullPaths), null));
+                tests.addAll(getAllNonTestGoFiles(parentDir));
             });
+
+            doLast(new TestPackagesAction(parentDirToFiles, true));
         }
     }
 
@@ -81,25 +102,132 @@ public class GoTestTask extends Go {
 
     private void addTestAllAction() {
         // https://golang.org/cmd/go/#hdr-Description_of_package_lists
-//        doLast(task -> buildManager.go(Arrays.asList("test", "./...", "-coverprofile=.gogradle/coverage.out"), null));
-
         Collection<File> allTestFiles = IOUtils.filterFilesRecursively(project.getRootDir(), TEST_GO_FILTER);
-        Map<File, List<File>> groupByParentDir = groupByParentDir(allTestFiles);
+        doLast(new TestPackagesAction(groupByParentDir(allTestFiles), false));
     }
 
     private Map<File, List<File>> groupByParentDir(Collection<File> files) {
         return files.stream()
-                .collect(Collectors.groupingBy(File::getParentFile));
+                .collect(groupingBy(File::getParentFile));
     }
 
-    private List<String> getAllNonTestGoFiles(File dir) {
-        List<String> names = safeList(dir);
-        return names.stream()
-                .filter(name -> name.endsWith(".go"))
-                .filter(name -> !StringUtils.startsWithAny(name, "_", "."))
-                .filter(name -> !name.endsWith("_test.go"))
-                .map(name -> new File(dir, name))
-                .map(File::getAbsolutePath)
+    private List<File> getAllNonTestGoFiles(File dir) {
+        return safeListFiles(dir).stream()
+                .filter(file -> file.getName().endsWith(".go"))
+                .filter(file -> !fileNameStartsWithAny(file, "_", "."))
+                .filter(file -> !fileNameEndsWithAny(file, "_test.go"))
+                .filter(File::isFile)
                 .collect(toList());
     }
+
+    private String dirToImportPath(File dir) {
+        Path relativeToProjectRoot = project.getRootDir().toPath().relativize(dir.toPath());
+        Path importPath = Paths.get(setting.getPackagePath()).resolve(relativeToProjectRoot);
+        return toUnixString(importPath);
+    }
+
+    private class TestPackagesAction implements Action<Task> {
+        private Map<File, List<File>> parentDirToTestFiles;
+
+        private boolean isCommandLineArguments;
+
+        private TestPackagesAction(Map<File, List<File>> parentDirToTestFiles, boolean isCommandLineArguments) {
+            this.parentDirToTestFiles = parentDirToTestFiles;
+            this.isCommandLineArguments = isCommandLineArguments;
+        }
+
+        @Override
+        public void execute(Task task) {
+            List<TestClassResult> testResults = new ArrayList<>();
+            RetcodeCollector retcodeCollector = new RetcodeCollector();
+
+            parentDirToTestFiles.forEach((parentDir, testFiles) -> {
+                String packageImportPath = dirToImportPath(parentDir);
+                StdoutStderrCollector stdoutCollector = doSingleTest(parentDir, testFiles, retcodeCollector);
+                PackageTestContext context = PackageTestContext.builder()
+                        .withPackagePath(packageImportPath)
+                        .withStdout(stdoutCollector.getStdoutStderr())
+                        .withTestFiles(testFiles)
+                        .build();
+
+                List<TestClassResult> resultOfSinglePackage = extractor.extractTestResult(context);
+                logResult(packageImportPath, resultOfSinglePackage);
+                testResults.addAll(resultOfSinglePackage);
+            });
+
+            GoTestResultsProvider provider = new GoTestResultsProvider(testResults);
+
+            File reportDir = new File(project.getRootDir(), "reports/test");
+            DefaultTestReport report = new DefaultTestReport(buildOperationProcessor);
+            report.generateReport(provider, reportDir);
+
+            if (retcodeCollector.containsNonZero()) {
+                throw new IllegalStateException("There are failed tests. Please see "
+                        + reportDir.getAbsolutePath()
+                        + " for more details");
+            }
+        }
+
+        private StdoutStderrCollector doSingleTest(File parentDir, List<File> testFiles, RetcodeCollector collector) {
+            StdoutStderrCollector lineConsumer = new StdoutStderrCollector();
+            if (isCommandLineArguments) {
+                List<String> args = CollectionUtils.asStringList(
+                        "test",
+                        "-v",
+                        testFiles.stream().map(File::getAbsolutePath).collect(toList()));
+                buildManager.go(args, null, lineConsumer, lineConsumer, collector);
+            } else {
+                String importPath = dirToImportPath(parentDir);
+                buildManager.go(asList("test", "-v", importPath), null, lineConsumer, lineConsumer, collector);
+            }
+
+            return lineConsumer;
+        }
+
+        private void logResult(String packagePath, List<TestClassResult> resultOfSinglePackage) {
+            LOGGER.quiet("Test for {} finished, {} succeed, {} failed",
+                    packagePath,
+                    successCount(resultOfSinglePackage),
+                    failureCount(resultOfSinglePackage)
+            );
+        }
+
+        private int successCount(List<TestClassResult> results) {
+            return results.stream()
+                    .mapToInt(result ->
+                            result.getResults().size() - result.getFailuresCount())
+                    .sum();
+        }
+
+        private int failureCount(List<TestClassResult> results) {
+            return results.stream().mapToInt(TestClassResult::getFailuresCount).sum();
+        }
+    }
+
+    private static class StdoutStderrCollector implements Consumer<String> {
+        private StringBuffer sb = new StringBuffer();
+
+        @Override
+        public void accept(String s) {
+            sb.append(s).append("\n");
+        }
+
+        public String getStdoutStderr() {
+            return sb.toString();
+        }
+    }
+
+    private static class RetcodeCollector implements Consumer<Integer> {
+        private List<Integer> retcodes = new ArrayList<>();
+
+        private boolean containsNonZero() {
+            return retcodes.stream().anyMatch(code -> code != 0);
+        }
+
+        @Override
+        public synchronized void accept(Integer code) {
+            retcodes.add(code);
+        }
+    }
+
 }
--- src/test/groovy/com/github/blindpirate/gogradle/task/GoTestTaskTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/task/GoTestTaskTest.groovy
@@ -2,16 +2,37 @@ package com.github.blindpirate.gogradle.task
 
 import com.github.blindpirate.gogradle.GogradleRunner
 import com.github.blindpirate.gogradle.support.WithResource
+import com.github.blindpirate.gogradle.task.go.GoTestStdoutExtractor
 import com.github.blindpirate.gogradle.task.go.GoTestTask
+import com.github.blindpirate.gogradle.task.go.PackageTestContext
 import com.github.blindpirate.gogradle.util.IOUtils
+import com.github.blindpirate.gogradle.util.ReflectionUtils
+import org.gradle.api.internal.tasks.testing.junit.result.TestClassResult
+import org.gradle.api.internal.tasks.testing.junit.result.TestMethodResult
+import org.gradle.api.logging.Logger
+import org.gradle.api.logging.Logging
+import org.gradle.api.tasks.testing.TestResult
+import org.gradle.internal.operations.BuildOperationProcessor
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Mock
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.stubbing.Answer
+
+import java.util.function.Consumer
 
 import static com.github.blindpirate.gogradle.task.GolangTaskContainer.INSTALL_BUILD_DEPENDENCIES_TASK_NAME
 import static com.github.blindpirate.gogradle.task.GolangTaskContainer.INSTALL_TEST_DEPENDENCIES_TASK_NAME
+import static org.mockito.ArgumentMatchers.any
 import static org.mockito.ArgumentMatchers.anyList
 import static org.mockito.ArgumentMatchers.anyMap
+import static org.mockito.ArgumentMatchers.anyString
+import static org.mockito.ArgumentMatchers.eq
+import static org.mockito.ArgumentMatchers.isNull
+import static org.mockito.Mockito.mock
 import static org.mockito.Mockito.times
 import static org.mockito.Mockito.verify
 import static org.mockito.Mockito.when
@@ -23,10 +44,42 @@ class GoTestTaskTest extends TaskTest {
 
     File resource
 
+    @Captor
+    ArgumentCaptor<List> argumentsCaptor
+
+    @Mock
+    GoTestStdoutExtractor extractor
+
+    @Mock
+    BuildOperationProcessor buildOperationProcessor
+
     @Before
     void setUp() {
         task = buildTask(GoTestTask)
         when(project.getRootDir()).thenReturn(resource)
+        when(setting.getPackagePath()).thenReturn('github.com/my/package')
+
+        ReflectionUtils.setField(task, 'extractor', extractor)
+        ReflectionUtils.setField(task, 'buildOperationProcessor', buildOperationProcessor)
+
+        IOUtils.write(resource, 'a/a1_test.go', '')
+        IOUtils.write(resource, 'a/_a1_test.go', '')
+        IOUtils.write(resource, 'a/a2_test.go', '')
+        IOUtils.write(resource, 'a/.a2_test.go', '')
+        IOUtils.write(resource, 'a/a1.go', '')
+        IOUtils.write(resource, 'a/a2.go', '')
+
+        IOUtils.write(resource, 'b/b1_test.go', '')
+        IOUtils.write(resource, 'b/b2_test.go', '')
+        IOUtils.write(resource, 'b/b1.go', '')
+        IOUtils.write(resource, 'b/b2.go', '')
+
+        IOUtils.mkdir(resource, 'c/testdata/c_test.go')
+        IOUtils.write(resource, 'c/c.go', '')
+        IOUtils.write(resource, 'c/c.nongo', '')
+
+        IOUtils.mkdir(resource, 'd')
+        IOUtils.mkdir(resource, 'vendor')
     }
 
     @Test
@@ -41,34 +94,89 @@ class GoTestTaskTest extends TaskTest {
         task.addDefaultActionIfNoCustomActions()
         task.actions[0].execute(task)
         // then
-        verify(buildManager).go(['test', './...', '-coverprofile=.gogradle/coverage.out'], null)
+        verify(buildManager, times(2)).go(argumentsCaptor.capture(), isNull(), any(Consumer), any(Consumer), any(Consumer))
+        assert argumentsCaptor.getAllValues().contains(['test', '-v', 'github.com/my/package/a'])
+        assert argumentsCaptor.getAllValues().contains(['test', '-v', 'github.com/my/package/b'])
     }
 
     @Test
-    void 'test with specific pattern should succeed'() {
-        // given
-        File a1 = IOUtils.write(resource, 'a/a1_test.go', '')
-        File a2 = IOUtils.write(resource, 'a/a2_test.go', '')
-        File a3 = IOUtils.write(resource, 'a/a3.go', '')
-        File b1 = IOUtils.write(resource, 'b/b1_test.go', '')
-        File b2 = IOUtils.write(resource, 'b/b2.go', '')
+    void 'specific package should be tested if pattern is set'() {
         // when
-        task.setTestNamePattern(['*_test*'])
+        ReflectionUtils.setField(task, 'testNamePattern', ['a1*'])
         task.addDefaultActionIfNoCustomActions()
-        task.actions.each { it.execute(task) }
+        task.actions[0].execute(task)
         // then
-        verify(buildManager).go(['test', b1.absolutePath, b2.absolutePath], null)
-        verify(buildManager).go(['test', a1.absolutePath, a2.absolutePath, a3.absolutePath], null)
+        verify(buildManager).go(argumentsCaptor.capture(), isNull(Map), any(Consumer), any(Consumer), any(Consumer))
+        List<String> args = argumentsCaptor.getValue()
+        assert args.size() == 5
+        assert args[0..1] == ['test', '-v']
+        assert ['a/a1_test.go', 'a/a1.go', 'a/a2.go'].any { args.contains(new File(resource, it).absolutePath) }
+    }
+
+    @Test
+    void 'collecting stdout should succeed'() {
+        // given
+        ReflectionUtils.setField(task, 'testNamePattern', ['a1*'])
+        when(buildManager.go(anyList(), isNull(Map), any(Consumer), any(Consumer), any(Consumer))).thenAnswer(new Answer<Object>() {
+            @Override
+            Object answer(InvocationOnMock invocation) throws Throwable {
+                invocation.arguments[2].accept('stdout')
+                invocation.arguments[3].accept('stderr')
+            }
+        })
+        ArgumentCaptor<PackageTestContext> argumentCaptor = ArgumentCaptor.forClass(PackageTestContext)
+        // when
+        task.addDefaultActionIfNoCustomActions()
+        task.actions[0].execute(task)
+        // then
+        verify(extractor).extractTestResult(argumentCaptor.capture())
+        PackageTestContext context = argumentCaptor.getValue()
+        assert context.packagePath == 'github.com/my/package/a'
+        assert context.stdout == 'stdout\nstderr\n'
+        assert context.testFiles.size() == 3
+    }
+
+    @Test
+    void 'successful and failed result should be counted correctly'() {
+        // given
+        TestClassResult result = new TestClassResult(1L, 'className', 2L)
+        result.add(new TestMethodResult(1L, 'methodName', TestResult.ResultType.FAILURE, 2L, 3L))
+        result.add(new TestMethodResult(1L, 'methodName', TestResult.ResultType.SUCCESS, 2L, 3L))
+        when(extractor.extractTestResult(any(PackageTestContext))).thenReturn([result])
+
+        Logger mockLogger = mock(Logger)
+        ReflectionUtils.setStaticFinalField(GoTestTask, 'LOGGER', mockLogger)
+        ReflectionUtils.setField(task, 'testNamePattern', ['a1*'])
+
+        // when
+        task.addDefaultActionIfNoCustomActions()
+        task.actions[0].execute(task)
+        // then
+        verify(mockLogger).quiet("Test for {} finished, {} succeed, {} failed", 'github.com/my/package/a', 1, 1)
+        ReflectionUtils.setStaticFinalField(GoTestTask, 'LOGGER', Logging.getLogger(GoTestTask))
+    }
+
+    @Test(expected = IllegalStateException)
+    void 'exception should be thrown if go test return non-zero'() {
+        // given
+        when(buildManager.go(anyList(), isNull(Map), any(Consumer), any(Consumer), any(Consumer))).thenAnswer(new Answer<Object>() {
+            @Override
+            Object answer(InvocationOnMock invocation) throws Throwable {
+                invocation.arguments[4].accept(0)
+                invocation.arguments[4].accept(1)
+            }
+        })
+        // when
+        task.addDefaultActionIfNoCustomActions()
+        task.actions[0].execute(task)
     }
 
     @Test
     void 'nothing should happened if no matched tests found'() {
         // when
-        task.setTestNamePattern(['*_test*'])
+        task.setTestNamePattern(['unexistent'])
         task.addDefaultActionIfNoCustomActions()
-        task.actions.each { it.execute(task) }
         // then
-        verify(buildManager, times(0)).go(anyList(), anyMap())
+        task.actions.size() == 0
     }
-
 }
