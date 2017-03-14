78931d0363895496267f51f81307b9380e7a5d66
Bo Zhang <zhangbodut@gmail.com>
2017-03-14 10:35:16 +0800

    Extractor that can extract information from go test stdout

---
+++ src/main/java/com/github/blindpirate/gogradle/task/go/GoTestStdoutExtractor.java
@@ -0,0 +1,156 @@
+package com.github.blindpirate.gogradle.task.go;
+
+import com.github.blindpirate.gogradle.util.IOUtils;
+import com.google.common.collect.ImmutableMap;
+import org.gradle.api.internal.tasks.testing.junit.result.TestClassResult;
+import org.gradle.api.internal.tasks.testing.junit.result.TestMethodResult;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
+import org.gradle.api.tasks.testing.TestResult;
+
+import javax.inject.Singleton;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.github.blindpirate.gogradle.util.DateUtils.toMilliseconds;
+import static java.lang.Double.parseDouble;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
+@Singleton
+public class GoTestStdoutExtractor {
+    private static final Logger LOGGER = Logging.getLogger(GoTestStdoutExtractor.class);
+    private static final Map<String, TestResult.ResultType> RESULT_TYPE_MAP =
+            ImmutableMap.of("PASS", TestResult.ResultType.SUCCESS,
+                    "FAIL", TestResult.ResultType.FAILURE);
+
+    //=== RUN   TestDiffToHTML
+    //--- PASS: TestDiffToHTML (0.00s)
+    private static final Pattern TEST_RESULT_PATTERN =
+            Pattern.compile("=== RUN\\s+(\\w+)\\n((?:.|\\n)*?)--- (PASS|FAIL):\\s+\\w+\\s+\\(((\\d+)(\\.\\d+)?)s\\)");
+
+    private static final String SETUP_FAILED_ERROR = "[setup failed]";
+    public static AtomicLong GLOBAL_COUNTER = new AtomicLong(0);
+
+    public List<TestClassResult> extractTestResult(PackageTestContext context) {
+        if (context.getStdout().contains(SETUP_FAILED_ERROR)) {
+            return setupFailedResult(context);
+        } else {
+            return successfulTestResults(context);
+        }
+    }
+
+    private List<TestClassResult> successfulTestResults(PackageTestContext context) {
+        Map<File, String> testFileContents = loadTestFiles(context.getTestFiles());
+        List<TestMethodResult> results = extractTestMethodResult(context.getStdout());
+
+        Map<File, List<TestMethodResult>> testFileToResults = groupByTestFile(results, testFileContents);
+
+        return testFileToResults.entrySet().stream()
+                .map(entry -> {
+                    String className = determineClassName(context.getPackagePath(), entry.getKey());
+                    return methodResultsToClassResult(context.getPackagePath(), className, entry.getValue());
+                })
+                .collect(toList());
+    }
+
+    private List<TestClassResult> setupFailedResult(PackageTestContext context) {
+        GoTestMethodResult result = new GoTestMethodResult(GLOBAL_COUNTER.incrementAndGet(),
+                SETUP_FAILED_ERROR,
+                TestResult.ResultType.FAILURE,
+                0L, 0L, context.getStdout()
+        );
+
+        return asList(methodResultsToClassResult(context.getPackagePath(), SETUP_FAILED_ERROR, asList(result)));
+    }
+
+    private List<TestMethodResult> extractTestMethodResult(String stdout) {
+        Matcher matcher = TEST_RESULT_PATTERN.matcher(stdout);
+        List<TestMethodResult> ret = new ArrayList<>();
+        while (matcher.find()) {
+            long id = GLOBAL_COUNTER.incrementAndGet();
+            String methodName = matcher.group(1);
+            String message = matcher.group(2);
+            TestResult.ResultType resultType = RESULT_TYPE_MAP.get(matcher.group(3));
+            long duration = toMilliseconds(parseDouble(matcher.group(4)));
+
+            TestMethodResult result = new GoTestMethodResult(id,
+                    methodName,
+                    resultType,
+                    duration,
+                    0L,
+                    message);
+            if (TestResult.ResultType.FAILURE == resultType) {
+                result.addFailure(message, message, message);
+            }
+            ret.add(result);
+        }
+        return ret;
+    }
+
+    private Map<File, String> loadTestFiles(List<File> testFiles) {
+        return testFiles.
+                stream()
+                .collect(toMap(Function.identity(), IOUtils::toString));
+    }
+
+    private TestClassResult methodResultsToClassResult(String packagePath,
+                                                       String className,
+                                                       List<TestMethodResult> methodResults) {
+        TestClassResult ret = new TestClassResult(GLOBAL_COUNTER.incrementAndGet(), className, 0L);
+        methodResults.forEach(ret::add);
+        return ret;
+    }
+
+    private String determineClassName(String packagePath, File testFile) {
+        String escapedPackagePath = IOUtils.encodeInternally(packagePath);
+        String fileName = testFile.getName();
+        String nameWithoutDotGo = testFile.getName().substring(0, fileName.length() - 3);
+
+        return escapedPackagePath.replaceAll("%2F", ".") + "." + nameWithoutDotGo;
+    }
+
+    private Map<File, List<TestMethodResult>> groupByTestFile(List<TestMethodResult> results, Map<File, String> testFiles) {
+        return results.stream()
+                .collect(groupingBy(
+                        result -> findTestFileOfMethod(testFiles, result.getName())
+                ));
+    }
+
+    private File findTestFileOfMethod(Map<File, String> testFileContents, String methodName) {
+        LOGGER.debug("trying to find {} in test files.");
+        return testFileContents
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().contains(methodName))
+                .findFirst()
+                .get()
+                .getKey();
+    }
+
+    public static class GoTestMethodResult extends TestMethodResult {
+        private String message;
+
+        public String getMessage() {
+            return message;
+        }
+
+        public GoTestMethodResult(long id,
+                                  String name,
+                                  TestResult.ResultType resultType,
+                                  long duration,
+                                  long endTime,
+                                  String message) {
+            super(id, name, resultType, duration, endTime);
+            this.message = message;
+        }
+    }
+}
---
+++ src/test/groovy/com/github/blindpirate/gogradle/task/go/GoTestStdoutExtractorTest.groovy
@@ -0,0 +1,117 @@
+package com.github.blindpirate.gogradle.task.go
+
+import com.github.blindpirate.gogradle.GogradleRunner
+import com.github.blindpirate.gogradle.support.WithResource
+import com.github.blindpirate.gogradle.util.IOUtils
+import org.gradle.api.internal.tasks.testing.junit.result.TestClassResult
+import org.gradle.api.tasks.testing.TestResult
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(GogradleRunner)
+@WithResource('')
+class GoTestStdoutExtractorTest {
+
+    GoTestStdoutExtractor extractor = new GoTestStdoutExtractor()
+
+    File resource
+
+    @Test
+    void 'extracting results from stdout should succeed'() {
+        // given
+        IOUtils.write(resource, 'a_test.go', 'func TestDiffToHTML()')
+        IOUtils.write(resource, 'b_test.go', 'func Test_parsePostgreSQLHostPort() func Test_SSHParsePublicKey()')
+        IOUtils.write(resource, 'c_test.go', 'func TestRepo()')
+        IOUtils.write(resource, 'd_test.go', 'func Useless()')
+        String stdout = '''
+2017/03/13 16:05:18 \u001B[1;33m[W] Custom config '/var/folders/y2/gy07kxsn58jdd97b43jcch040000gn/T/go-build158292335/github.com/gogits/gogs/models/_test/custom/conf/app.ini' not found, ignore this if you're running first time
+=== RUN   TestDiffToHTML
+--- PASS: TestDiffToHTML (0.00s)
+=== RUN   Test_parsePostgreSQLHostPort
+
+  Parse PostgreSQL host and port ✔✔✔✔✔✔✔✔✔✔✔✔
+
+
+12 total assertions
+
+--- PASS: Test_parsePostgreSQLHostPort (0.00s)
+=== RUN   Test_SSHParsePublicKey
+
+  Parse public keys in both native and ssh-keygen 
+Testing key: ecdsa-256
+✔✔✔✔✔
+Testing key: ecdsa-384
+✔✔✔✔✔
+Testing key: dsa-1024
+✔✔✘
+
+
+Failures:
+
+  * /Users/zhb/Develop/top1000Go/gogits_gogs/.gogradle/project_gopath/src/github.com/gogits/gogs/models/ssh_key_test.go 
+  Line 42:
+  Expected: '10241\'
+  Actual:   '1024\'
+  (Should be equal)
+
+
+25 total assertions
+
+--- FAIL: Test_SSHParsePublicKey (0.04s)
+=== RUN   TestRepo
+
+  The metas map 
+    When no external tracker is configured 
+      It should be nil ✔
+      It should be nil even if other settings are present ✔
+    When an external issue tracker is configured 
+      It should default to numeric issue style ✔
+      It should pass through numeric issue style setting ✔
+      It should pass through alphanumeric issue style setting ✔
+      It should contain the user name ✔
+      It should contain the repo name ✔
+      It should contain the URL format ✔
+
+
+33 total assertions
+
+--- PASS: TestRepo (0.00s)
+FAIL
+exit status 1
+FAIL\tgithub.com/gogits/gogs/models\t0.074s
+'''
+        // when
+        PackageTestContext context = PackageTestContext.builder()
+                .withPackageDirectory(resource)
+                .withPackagePath('github.com/gogits/gogs/models')
+                .withStdout(stdout)
+                .withTestFiles(resource.listFiles() as List)
+                .build()
+        // Sort to ensure the order
+        List<TestClassResult> results = extractor.extractTestResult(context).sort({ result1, result2 ->
+            result1.className <=> result2.className
+        })
+
+        // then
+        assert results.size() == 3
+
+        assert results[0].results.size() == 1
+        assert results[0].results[0].name == 'TestDiffToHTML'
+        assert results[0].results[0].message == ''
+        assert results[0].results[0].resultType == TestResult.ResultType.SUCCESS
+
+        assert results[1].results.size() == 2
+        assert results[1].results[0].name == 'Test_parsePostgreSQLHostPort'
+        assert results[1].results[0].resultType == TestResult.ResultType.SUCCESS
+        assert results[1].results[0].message.contains('Parse PostgreSQL host and port')
+        assert results[1].results[1].name == 'Test_SSHParsePublicKey'
+        assert results[1].results[1].resultType == TestResult.ResultType.FAILURE
+        assert results[1].results[1].message.contains('Failures:')
+
+        assert results[2].results.size() == 1
+        assert results[2].results[0].name == 'TestRepo'
+        assert results[2].results[0].message.contains('33 total assertions')
+        assert results[2].results[0].resultType == TestResult.ResultType.SUCCESS
+
+    }
+}
