ede9b37cf24d89dbd31a2daa377d227b351feed3
Bo Zhang <zhangbodut@gmail.com>
2017-03-14 15:23:39 +0800

    Custom implementation of TestResultsProvider to render Go test report

---
+++ src/main/java/com/github/blindpirate/gogradle/task/go/GoTestResultsProvider.java
@@ -0,0 +1,80 @@
+package com.github.blindpirate.gogradle.task.go;
+
+import com.github.blindpirate.gogradle.util.ExceptionHandler;
+import com.github.blindpirate.gogradle.util.StringUtils;
+import org.gradle.api.Action;
+import org.gradle.api.internal.tasks.testing.junit.result.TestClassResult;
+import org.gradle.api.internal.tasks.testing.junit.result.TestResultsProvider;
+import org.gradle.api.tasks.testing.TestOutputEvent;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.github.blindpirate.gogradle.task.go.GoTestStdoutExtractor.GoTestMethodResult;
+
+public class GoTestResultsProvider implements TestResultsProvider {
+    private List<TestClassResult> testClassResults = new ArrayList<>();
+
+    private Map<Long, TestClassResult> idToClassResultMap = new HashMap<>();
+
+    public GoTestResultsProvider(List<TestClassResult> results) {
+        testClassResults.addAll(results);
+        testClassResults.forEach(result -> idToClassResultMap.put(result.getId(), result));
+    }
+
+    @Override
+    public void writeAllOutput(long id, TestOutputEvent.Destination destination, Writer writer) {
+        TestClassResult result = idToClassResultMap.get(id);
+        String stdout = result.getResults().stream()
+                .map(methodResult -> (GoTestMethodResult) methodResult)
+                .map(GoTestMethodResult::getMessage)
+                .filter(StringUtils::isNotEmpty)
+                .collect(Collectors.joining("\n"));
+        try {
+            writer.write(stdout);
+        } catch (IOException e) {
+            throw ExceptionHandler.uncheckException(e);
+        }
+    }
+
+    @Override
+    public void writeNonTestOutput(long id, TestOutputEvent.Destination destination, Writer writer) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void writeTestOutput(long classId, long testId, TestOutputEvent.Destination destination, Writer writer) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitClasses(Action<? super TestClassResult> visitor) {
+        testClassResults.forEach(visitor::execute);
+    }
+
+    @Override
+    public boolean hasOutput(long id, TestOutputEvent.Destination destination) {
+        if (destination == TestOutputEvent.Destination.StdErr) {
+            return false;
+        }
+        TestClassResult result = idToClassResultMap.get(id);
+        return result.getResults().stream()
+                .map(methodResult -> (GoTestMethodResult) methodResult)
+                .map(GoTestMethodResult::getMessage)
+                .anyMatch(StringUtils::isNotEmpty);
+    }
+
+    @Override
+    public boolean isHasResults() {
+        return !testClassResults.isEmpty();
+    }
+
+    @Override
+    public void close() throws IOException {
+    }
+}
---
+++ src/test/groovy/com/github/blindpirate/gogradle/task/go/GoTestResultsProviderTest.groovy
@@ -0,0 +1,94 @@
+package com.github.blindpirate.gogradle.task.go
+
+import com.github.blindpirate.gogradle.GogradleRunner
+import org.gradle.api.Action
+import org.gradle.api.internal.tasks.testing.junit.result.TestClassResult
+import org.gradle.api.tasks.testing.TestOutputEvent
+import org.gradle.api.tasks.testing.TestResult
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+
+import static com.github.blindpirate.gogradle.task.go.GoTestStdoutExtractor.GoTestMethodResult
+import static org.mockito.ArgumentMatchers.anyString
+import static org.mockito.Mockito.mock
+import static org.mockito.Mockito.verify
+import static org.mockito.Mockito.when
+
+@RunWith(GogradleRunner)
+class GoTestResultsProviderTest {
+
+    GoTestResultsProvider provider
+
+    List<TestClassResult> classResults
+
+    @Mock
+    Writer writer
+
+    @Before
+    void setUp() {
+        TestClassResult classResult1 = new TestClassResult(1L, 'className', 2L)
+        GoTestMethodResult methodResult11 = new GoTestMethodResult(3L, 'methodName1', TestResult.ResultType.SUCCESS, 4L, 5L, '')
+        GoTestMethodResult methodResult12 = new GoTestMethodResult(6L, 'methodName2', TestResult.ResultType.FAILURE, 6L, 7L, 'failure message')
+        classResult1.add(methodResult11)
+        classResult1.add(methodResult12)
+
+        TestClassResult classResult2 = new TestClassResult(11L, 'className2', 12L)
+        classResult2.add(new GoTestMethodResult(13L, 'methodName3', TestResult.ResultType.SUCCESS, 14L, 15L, '  '))
+
+        classResults = [classResult1, classResult2]
+        provider = new GoTestResultsProvider(classResults)
+    }
+
+    @Test(expected = UnsupportedOperationException)
+    void 'writeNonTestOutput should be unsupported'() {
+        new GoTestResultsProvider([]).writeNonTestOutput(0L, null, null)
+    }
+
+    @Test(expected = UnsupportedOperationException)
+    void 'writeTestOutput should be unsupported'() {
+        new GoTestResultsProvider([]).writeTestOutput(0L, 0L, null, null)
+    }
+
+    @Test
+    void 'write all output of class should succeed'() {
+        // when
+        provider.writeAllOutput(1L, TestOutputEvent.Destination.StdOut, writer)
+        // then
+        writer.write('failure message')
+    }
+
+    @Test(expected = IllegalStateException)
+    void 'exception should be thrown if writer throws IOException'(){
+        when(writer.write(anyString())).thenThrow(IOException)
+        provider.writeAllOutput(1L, TestOutputEvent.Destination.StdOut, writer)
+    }
+
+    @Test
+    void 'visitClasses should succeed'() {
+        Action action = mock(Action)
+        provider.visitClasses(action)
+        verify(action).execute(classResults[0])
+        verify(action).execute(classResults[1])
+    }
+
+    @Test
+    void 'hasOutput should succeed'() {
+        assert !provider.hasOutput(1L, TestOutputEvent.Destination.StdErr)
+        assert !provider.hasOutput(11L, TestOutputEvent.Destination.StdErr)
+        assert provider.hasOutput(1L, TestOutputEvent.Destination.StdOut)
+        assert provider.hasOutput(11L, TestOutputEvent.Destination.StdOut)
+    }
+
+    @Test
+    void 'isHasResults should succeed'() {
+        assert provider.isHasResults()
+        assert !new GoTestResultsProvider([]).isHasResults()
+    }
+
+    @Test
+    void 'close should succeed'() {
+        provider.close()
+    }
+}
