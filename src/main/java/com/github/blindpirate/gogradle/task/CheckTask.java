425495372f165871e4e7682fce4294591cf5d8e3
Bo Zhang <zhangbodut@gmail.com>
2017-03-17 21:52:01 +0800

    Check task depend fmt and vet task

--- src/main/java/com/github/blindpirate/gogradle/task/CheckTask.java
+++ src/main/java/com/github/blindpirate/gogradle/task/CheckTask.java
@@ -1,10 +1,12 @@
 package com.github.blindpirate.gogradle.task;
 
+import static com.github.blindpirate.gogradle.task.GolangTaskContainer.GOFMT_TASK_NAME;
+import static com.github.blindpirate.gogradle.task.GolangTaskContainer.GOVET_TASK_NAME;
 import static com.github.blindpirate.gogradle.task.GolangTaskContainer.TEST_TASK_NAME;
 
 public class CheckTask extends AbstractGolangTask {
 
     public CheckTask() {
-        dependsOn(TEST_TASK_NAME);
+        dependsOn(TEST_TASK_NAME, GOFMT_TASK_NAME, GOVET_TASK_NAME);
     }
 }
