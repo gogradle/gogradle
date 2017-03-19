364e79c5e94f4847315b85027b7442090efb0f43
Bo Zhang <zhangbodut@gmail.com>
2017-03-20 07:32:43 +0800

    Debug on Windows

--- src/test/groovy/com/github/blindpirate/gogradle/support/OnlyWhenProcessor.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/support/OnlyWhenProcessor.groovy
@@ -1,19 +1,28 @@
 package com.github.blindpirate.gogradle.support
 
+import org.gradle.api.logging.Logger
+import org.gradle.api.logging.Logging
 import org.junit.runners.model.FrameworkMethod
 
 class OnlyWhenProcessor extends GogradleRunnerProcessor<OnlyWhen> {
+    private static final Logger LOGGER = Logging.getLogger(OnlyWhenProcessor.class)
+
     @Override
     boolean shouldIgnore(FrameworkMethod method, OnlyWhen annotation) {
         try {
-            return !new GroovyShell().evaluate(annotation.value())
+            Object ret = !new GroovyShell().evaluate(annotation.value())
+            LOGGER.info("result: {}", ret)
+            return ret
         } catch (Exception e) {
             switch (annotation.ignoreTestWhenException()) {
                 case OnlyWhen.ExceptionStrategy.TRUE:
+                    LOGGER.info("exception with true strategy")
                     return true
                 case OnlyWhen.ExceptionStrategy.FALSE:
+                    LOGGER.info("exception with false strategy")
                     return false
                 default:
+                    LOGGER.info("exception with default strategy")
                     throw e
             }
         }
