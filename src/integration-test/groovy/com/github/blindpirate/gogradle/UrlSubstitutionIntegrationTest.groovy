09f27f174a4fad73035afee10895efa8b29ab60f
Bo Zhang <zhangbodut@gmail.com>
2017-03-25 19:37:33 +0800

    Integration test for url substitution

---
+++ src/integration-test/groovy/com/github/blindpirate/gogradle/UrlSubstitutionIntegrationTest.groovy
@@ -0,0 +1,57 @@
+package com.github.blindpirate.gogradle
+
+import com.github.blindpirate.gogradle.support.AccessWeb
+import com.github.blindpirate.gogradle.support.IntegrationTestSupport
+import com.github.blindpirate.gogradle.support.WithResource
+import com.github.blindpirate.gogradle.util.IOUtils
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(GogradleRunner)
+@WithResource('')
+class UrlSubstitutionIntegrationTest extends IntegrationTestSupport {
+
+    File resource
+
+    @Override
+    File getProjectRoot() {
+        return resource
+    }
+
+    String buildDotGradle = """
+${buildDotGradleBase}
+
+golang {
+    packagePath='my/project'
+}
+
+repositories {
+    golang {
+        all()
+        url {
+            'https://github.com/blindpirate/test-for-gogradle.git'
+        }
+    }
+}
+
+dependencies {
+    build 'my/awesome/project'
+}
+"""
+
+    @Test
+    @AccessWeb
+    void 'url substitution should succeed'() {
+        IOUtils.write(resource, 'build.gradle', buildDotGradle)
+        try {
+            newBuild {
+                it.forTasks('installBuildDependencies')
+            }
+        } finally {
+            println(stderr)
+            println(stdout)
+        }
+
+        assert new File(resource, ".gogradle/build_gopath/src/my/awesome/project/helloworld.go").exists()
+    }
+}
