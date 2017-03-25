73fb0e172a455cf5cbe9d6edf1ec31d3de5c594d
Bo Zhang <zhangbodut@gmail.com>
2017-03-25 18:14:07 +0800

    Extract common method to parse raw time in git and hg output

--- src/main/java/com/github/blindpirate/gogradle/util/DateUtils.java
+++ src/main/java/com/github/blindpirate/gogradle/util/DateUtils.java
@@ -15,4 +15,12 @@ public class DateUtils {
     public static String formatNow(String pattern) {
         return new SimpleDateFormat(pattern).format(new Date());
     }
+
+    public static long parseRaw(String raw) {
+        // git and hg raw time
+        // 1481274259 +0800
+        String[] unixSecondAndTimezone = raw.split(" ");
+        Assert.isTrue(unixSecondAndTimezone.length == 2, "Illegal raw time: " + raw);
+        return toMilliseconds(Long.parseLong(unixSecondAndTimezone[0]));
+    }
 }
