c57a92e72dcd7788e2fd54602f3dca22348e8c17
Bo Zhang <zhangbodut@gmail.com>
2017-03-30 12:18:00 +0800

    Refactor MercurialResolvedDependency

--- src/test/groovy/com/github/blindpirate/gogradle/vcs/mercurial/MercurialResolvedDependencyTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/vcs/mercurial/MercurialResolvedDependencyTest.groovy
@@ -1,16 +1,28 @@
 package com.github.blindpirate.gogradle.vcs.mercurial
 
+import com.github.blindpirate.gogradle.GogradleGlobal
+import com.github.blindpirate.gogradle.GogradleRunner
+import com.github.blindpirate.gogradle.core.dependency.install.DependencyInstaller
+import com.github.blindpirate.gogradle.support.WithMockInjector
+import com.github.blindpirate.gogradle.vcs.Mercurial
 import com.github.blindpirate.gogradle.vcs.VcsType
+import com.google.inject.Key
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
 
+@RunWith(GogradleRunner)
 class MercurialResolvedDependencyTest {
     @Test
+    @WithMockInjector
     void 'correct installer class should be returned'() {
-        assert new MercurialResolvedDependency(null, null, 0L).installerClass == MercurialDependencyManager.class
+        DependencyInstaller installer = Mockito.mock(DependencyInstaller)
+        Mockito.when(GogradleGlobal.INSTANCE.getInjector().getInstance(Key.get(DependencyInstaller, Mercurial))).thenReturn(installer)
+        assert new MercurialResolvedDependency(null, null, null, 0L).installer == installer
     }
 
     @Test
     void 'correct vcs type should be returned'() {
-        assert new MercurialResolvedDependency(null, null, 0L).vcsType == VcsType.MERCURIAL
+        assert new MercurialResolvedDependency(null, null, null, 0L).vcsType == VcsType.MERCURIAL
     }
 }
