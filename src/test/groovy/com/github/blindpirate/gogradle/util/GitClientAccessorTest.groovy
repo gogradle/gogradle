f97f434f891ca9c067b3ddaf7195a73afb368a7a
Bo Zhang <zhangbodut@gmail.com>
2017-04-01 16:23:23 +0800

    Fix tag order issue

--- src/test/groovy/com/github/blindpirate/gogradle/util/GitClientAccessorTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/util/GitClientAccessorTest.groovy
@@ -137,7 +137,7 @@ class GitClientAccessorTest {
         List<GitMercurialCommit> commits = accessor.getAllTags(resource)
         assert commits.collect {
             it.tag
-        } == ['3.0.0', '2.1.2', '2.1.1', '2.1.0', '2.0', '1.2.0', 'unknown_tag', '1.0.0', '0.0.3-prerelease', 'v0.0.2', '0.0.1']
+        } == ['3.0.0', '2.1.2', '2.1.1', '2.1.0', '2.0', '1.2.0', '1.0.0', '0.0.3-prerelease', 'v0.0.2', '0.0.1']
     }
 
     @Test
