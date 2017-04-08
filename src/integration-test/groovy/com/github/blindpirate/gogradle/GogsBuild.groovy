603477c56c7572745c9233100d3e7f633ef249ba
Bo Zhang <zhangbodut@gmail.com>
2017-04-08 16:37:24 +0800

    test -> goTest

--- src/integration-test/groovy/com/github/blindpirate/gogradle/GogsBuild.groovy
+++ src/integration-test/groovy/com/github/blindpirate/gogradle/GogsBuild.groovy
@@ -46,7 +46,7 @@ goVet {
 
         // I don't know why it will fail on Windows
         if (Os.getHostOs() == Os.WINDOWS) {
-            writeBuildAndSettingsDotGradle(buildDotGradle + 'test.enabled = false')
+            writeBuildAndSettingsDotGradle(buildDotGradle + 'goTest.enabled = false')
         } else {
             writeBuildAndSettingsDotGradle(buildDotGradle)
         }
