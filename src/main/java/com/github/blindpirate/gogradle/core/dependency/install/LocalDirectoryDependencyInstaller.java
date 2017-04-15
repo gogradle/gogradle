cee140971923aaec88133b305a4fa34d4882f15d
Bo Zhang <zhangbodut@gmail.com>
2017-04-15 17:03:06 +0800

    Remove common methods

--- src/main/java/com/github/blindpirate/gogradle/core/dependency/install/LocalDirectoryDependencyInstaller.java
+++ src/main/java/com/github/blindpirate/gogradle/core/dependency/install/LocalDirectoryDependencyInstaller.java
@@ -2,7 +2,6 @@ package com.github.blindpirate.gogradle.core.dependency.install;
 
 import com.github.blindpirate.gogradle.core.dependency.LocalDirectoryDependency;
 import com.github.blindpirate.gogradle.core.dependency.ResolvedDependency;
-import com.github.blindpirate.gogradle.core.dependency.VendorResolvedDependency;
 import com.github.blindpirate.gogradle.util.IOUtils;
 
 import javax.inject.Singleton;
@@ -10,29 +9,12 @@ import java.io.File;
 import java.nio.file.Path;
 
 @Singleton
-public class LocalDirectoryDependencyInstaller implements DependencyInstaller {
+public class LocalDirectoryDependencyInstaller implements VendorSupportDependencyInstallerMixin {
     @Override
     public void install(ResolvedDependency dependency, File targetDirectory) {
         LocalDirectoryDependency realDependency = (LocalDirectoryDependency) determineDependency(dependency);
         Path realPath = realDependency.getRootDir().toPath().resolve(determineRelativePath(dependency));
 
         IOUtils.copyDirectory(realPath.toFile(), targetDirectory, DependencyInstallFileFilter.INSTANCE);
-    }
-
-    // TODO duplicated code
-    private ResolvedDependency determineDependency(ResolvedDependency dependency) {
-        if (dependency instanceof VendorResolvedDependency) {
-            return VendorResolvedDependency.class.cast(dependency).getHostDependency();
-        } else {
-            return dependency;
-        }
-    }
-
-    private String determineRelativePath(ResolvedDependency dependency) {
-        if (dependency instanceof VendorResolvedDependency) {
-            return VendorResolvedDependency.class.cast(dependency).getRelativePathToHost();
-        } else {
-            return ".";
-        }
     }
 }
