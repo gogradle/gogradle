ee17a53c0907b5f23f34aa1ea4adb46bb1dab41c
Bo Zhang <zhangbodut@gmail.com>
2017-04-17 10:43:10 +0800

    Complete DirMapNoatationParserTest

--- src/test/groovy/com/github/blindpirate/gogradle/core/dependency/parse/DirMapNotationParserTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/core/dependency/parse/DirMapNotationParserTest.groovy
@@ -1,11 +1,14 @@
 package com.github.blindpirate.gogradle.core.dependency.parse
 
 import com.github.blindpirate.gogradle.GogradleRunner
+import com.github.blindpirate.gogradle.core.LocalDirectoryGolangPackage
 import com.github.blindpirate.gogradle.core.dependency.GolangDependency
 import com.github.blindpirate.gogradle.core.dependency.LocalDirectoryDependency
 import com.github.blindpirate.gogradle.support.WithResource
 import org.junit.Test
 import org.junit.runner.RunWith
+
+import static com.github.blindpirate.gogradle.util.StringUtils.toUnixString
 
 @RunWith(GogradleRunner)
 @WithResource('')
@@ -23,5 +26,15 @@ class DirMapNotationParserTest {
         assert dependency instanceof LocalDirectoryDependency
         assert dependency.name == 'path'
         assert dependency.rootDir == resource
+    }
+
+    @Test
+    void 'parsing dependency with LocalDirectoryGolangPackage should succeed'() {
+        // when
+        GolangDependency result = parser.parse([name: 'local', package: LocalDirectoryGolangPackage.of('local', 'local', toUnixString(resource))])
+        // then
+        assert result instanceof LocalDirectoryDependency
+        assert result.name == 'local'
+        assert result.rootDir == resource
     }
 }
