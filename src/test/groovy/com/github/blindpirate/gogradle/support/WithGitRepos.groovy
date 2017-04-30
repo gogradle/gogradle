c2b1a2f8adc2bc0f549d70b124ce71175c28f93e
Bo Zhang <zhangbodut@gmail.com>
2017-04-30 15:30:56 +0800

    Support literal values in @GitRepos

--- src/test/groovy/com/github/blindpirate/gogradle/support/WithGitRepos.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/support/WithGitRepos.groovy
@@ -8,5 +8,9 @@ import java.lang.annotation.Target
 @Retention(RetentionPolicy.RUNTIME)
 @Target([ElementType.METHOD, ElementType.TYPE])
 @interface WithGitRepos {
-    String value()
+    String value() default ""
+
+    String[] fileNames() default []
+
+    String[] repoNames() default []
 }
