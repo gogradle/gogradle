e612760ad0570f03d22588444d794330beda4e4e
Bo Zhang <zhangbodut@gmail.com>
2017-05-04 06:37:27 +0800

    Remove gogradle.lock before goLock task

--- src/main/java/com/github/blindpirate/gogradle/task/PrepareTask.java
+++ src/main/java/com/github/blindpirate/gogradle/task/PrepareTask.java
@@ -5,16 +5,21 @@ import com.github.blindpirate.gogradle.build.BuildManager;
 import com.github.blindpirate.gogradle.core.BuildConstraintManager;
 import com.github.blindpirate.gogradle.core.dependency.GogradleRootProject;
 import com.github.blindpirate.gogradle.crossplatform.GoBinaryManager;
+import com.github.blindpirate.gogradle.util.IOUtils;
 import org.gradle.api.DefaultTask;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
 import org.gradle.api.tasks.TaskAction;
 
 import javax.inject.Inject;
+import java.io.File;
 
 /**
  * This task perform preparation such as Go executable and GOPATH.
  */
 public class PrepareTask extends DefaultTask {
 
+    private static final Logger LOGGER = Logging.getLogger(PrepareTask.class);
     @Inject
     private GoBinaryManager goBinaryManager;
 
@@ -38,6 +43,16 @@ public class PrepareTask extends DefaultTask {
         buildManager.prepareSymbolicLinks();
         buildConstraintManager.prepareConstraints();
         gogradleRootProject.initSingleton(setting.getPackagePath(), getProject().getRootDir());
+        deleteGogradleDotLockIfLockTaskExists();
+    }
+
+    private void deleteGogradleDotLockIfLockTaskExists() {
+        File gogradleDotLock = new File(getProject().getRootDir(), "gogradle.lock");
+        if (getProject().getTasks().getByName(GolangTaskContainer.LOCK_TASK_NAME) != null
+                && gogradleDotLock.exists()) {
+            LOGGER.warn("gogradle.lock already exists, it will be removed now.");
+            IOUtils.forceDelete(gogradleDotLock);
+        }
     }
 
 }
--- src/test/groovy/com/github/blindpirate/gogradle/task/PrepareTaskTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/task/PrepareTaskTest.groovy
@@ -2,12 +2,15 @@ package com.github.blindpirate.gogradle.task
 
 import com.github.blindpirate.gogradle.GogradleRunner
 import com.github.blindpirate.gogradle.support.WithResource
+import com.github.blindpirate.gogradle.util.IOUtils
+import org.gradle.api.Task
+import org.gradle.api.internal.tasks.TaskContainerInternal
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
 
-import static org.mockito.Mockito.verify
-import static org.mockito.Mockito.when
+import static org.mockito.Mockito.*
 
 @RunWith(GogradleRunner)
 @WithResource('')
@@ -17,10 +20,14 @@ class PrepareTaskTest extends TaskTest {
 
     File resource
 
+    @Mock
+    TaskContainerInternal taskContainer
+
     @Before
     void setUp() {
         task = buildTask(PrepareTask)
         when(project.getRootDir()).thenReturn(resource)
+        when(project.getTasks()).thenReturn(taskContainer)
         when(setting.getPackagePath()).thenReturn('github.com/my/project')
     }
 
@@ -35,6 +42,17 @@ class PrepareTaskTest extends TaskTest {
         verify(buildManager).prepareSymbolicLinks()
         verify(buildConstraintManager).prepareConstraints()
         verify(gogradleRootProject).initSingleton('github.com/my/project', resource)
+    }
+
+    @Test
+    void 'old gogradle.lock should be removed before locking'() {
+        // given
+        IOUtils.write(resource, 'gogradle.lock', '')
+        when(taskContainer.getByName('goLock')).thenReturn(mock(Task))
+        // when
+        task.prepare()
+        // then
+        assert !new File(resource, 'gogradle.lock').exists()
     }
 
 }
