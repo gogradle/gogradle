657d26c8c8c1a8e96101c4a1468e08b1284d47cb
Bo Zhang <zhangbodut@gmail.com>
2017-05-18 08:46:55 +0800

    Refactor InstallTask
    Signed-off-by: Bo Zhang <zhangbodut@gmail.com>

--- src/test/groovy/com/github/blindpirate/gogradle/task/InstallTaskTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/task/InstallTaskTest.groovy
@@ -21,129 +21,93 @@ import com.github.blindpirate.gogradle.GogradleRunner
 import com.github.blindpirate.gogradle.core.dependency.GolangDependencySet
 import com.github.blindpirate.gogradle.core.dependency.LocalDirectoryDependency
 import com.github.blindpirate.gogradle.core.dependency.ResolvedDependency
-import com.github.blindpirate.gogradle.core.dependency.VendorResolvedDependency
-import com.github.blindpirate.gogradle.core.dependency.tree.DependencyTreeNode
 import com.github.blindpirate.gogradle.support.WithResource
-import com.github.blindpirate.gogradle.util.DependencyUtils
 import com.github.blindpirate.gogradle.util.IOUtils
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
 
-import java.nio.file.Path
-
+import static com.github.blindpirate.gogradle.util.DependencyUtils.*
 import static org.mockito.Mockito.*
 
 @RunWith(GogradleRunner)
 @WithResource('')
 class InstallTaskTest extends TaskTest {
 
-    InstallBuildDependenciesTask installBuildDependenciesTask
-    InstallTestDependenciesTask installTestDependenciesTask
-    GolangDependencySet golangDependencySet = GolangDependencySet.empty()
+    InstallTask task
 
-    @Mock
-    DependencyTreeNode dependencyTree
-
-    ResolvedDependency resolvedDependency = DependencyUtils.mockResolvedDependency('name')
+    ResolvedDependency resolvedDependency = mockResolvedDependency('name')
 
     File resource
 
     @Before
     void setUp() {
-        installBuildDependenciesTask = buildTask(InstallBuildDependenciesTask)
-        installTestDependenciesTask = buildTask(InstallTestDependenciesTask)
-        when(golangTaskContainer.get(ResolveBuildDependenciesTask).getConfigurationName()).thenReturn('build')
-        when(golangTaskContainer.get(ResolveTestDependenciesTask).getConfigurationName()).thenReturn('test')
-        golangDependencySet.add(resolvedDependency)
-    }
-
-    @Test
-    void 'getting serialization file should succeed'() {
-        installBuildDependenciesTask.getSerializationFile()
-        installTestDependenciesTask.getSerializationFile()
-        verify(golangTaskContainer.get(ResolveBuildDependenciesTask)).getSerializationFile()
-        verify(golangTaskContainer.get(ResolveTestDependenciesTask)).getSerializationFile()
-    }
-
-    @Test
-    void 'getting installation directory should succeed'() {
-        // given
-        Path buildGopath = mock(Path)
-        Path testGopath = mock(Path)
-        when(buildManager.getInstallationDirectory('build')).thenReturn(buildGopath)
-        when(buildManager.getInstallationDirectory('test')).thenReturn(testGopath)
-
-        // then
-        installBuildDependenciesTask.getInstallationDirectory()
-        verify(buildGopath).toFile()
-        installTestDependenciesTask.getInstallationDirectory()
-        verify(testGopath).toFile()
+        when(project.getRootDir()).thenReturn(resource)
+        task = buildTask(InstallTask)
     }
 
     /*
-    INSTALLATION_DIR
-    \-- src
+   projectRoot
+    \--vendor
         |-- github.com
         |   \-- user
-        |       |-- a VERSON exist, match
-        |       |-- b VERSON exist, not match
-        |       |-- c VERSON not exist, .go exist
-        |       |-- d VESION not exist, .go not exist
-        |       \-- e VESION exist
+        |       |-- a up-to-date
+        |       |   \- vendor
+        |       |-- b out-of-date
+        |       \-- c not exist
         |
-        |-- f VERSION exist local directory
-        \-- g VERSION exist vendor of local
+        \-- f local directory
      */
 
     @Test
     void 'installation should succeed'() {
         // given
-        when(buildManager.getInstallationDirectory('build')).thenReturn(resource.toPath())
-        IOUtils.write(resource, 'src/github.com/user/a/.CURRENT_VERSION', 'a')
-        IOUtils.write(resource, 'src/github.com/user/b/.CURRENT_VERSION', 'unmatchedB')
-        IOUtils.write(resource, 'src/github.com/user/c/main.go', '')
-        IOUtils.write(resource, 'src/github.com/user/d/main.c', '')
-        IOUtils.write(resource, 'src/github.com/user/e/.CURRENT_VERSION', 'e')
-        IOUtils.write(resource, 'src/f/.CURRENT_VERSION', 'whatever')
-        IOUtils.write(resource, 'src/g/.CURRENT_VERSION', 'whatever')
-        ResolvedDependency a = DependencyUtils.mockResolvedDependency('github.com/user/a')
-        ResolvedDependency b = DependencyUtils.mockResolvedDependency('github.com/user/b')
-        ResolvedDependency c = DependencyUtils.mockResolvedDependency('github.com/user/c')
-        ResolvedDependency d = DependencyUtils.mockResolvedDependency('github.com/user/d')
-        when(a.getVersion()).thenReturn('a')
-        when(b.getVersion()).thenReturn('b')
-        when(c.getVersion()).thenReturn('c')
-        when(d.getVersion()).thenReturn('d')
+        ['a', 'b', 'c'].each { IOUtils.write(resource, "vendor/github.com/user/${it}/${it}.go", '') }
+        ['f'].each { IOUtils.write(resource, "vendor/${it}/${it}.go", '') }
 
-        LocalDirectoryDependency f = DependencyUtils.mockWithName(LocalDirectoryDependency, 'f')
-        VendorResolvedDependency g = DependencyUtils.mockWithName(VendorResolvedDependency, 'g')
-        when(f.getVersion()).thenReturn('f')
-        when(g.getVersion()).thenReturn('g')
-        when(g.getHostDependency()).thenReturn(f)
 
-        GolangDependencySet dependencies = DependencyUtils.asGolangDependencySet(a, b, c, d, f, g)
-        when(golangTaskContainer.get(ResolveBuildDependenciesTask).getDependencyTree()).thenReturn(dependencyTree)
-        when(dependencyTree.flatten()).thenReturn(dependencies)
+        ResolvedDependency a = mockResolvedDependency('github.com/user/a')
+        ResolvedDependency b = mockResolvedDependency('github.com/user/b')
+        when(vendorSnapshoter.isUpToDate(a, new File(resource, 'vendor/github.com/user/a'))).thenReturn(true)
+        when(vendorSnapshoter.isUpToDate(b, new File(resource, 'vendor/github.com/user/b'))).thenReturn(false)
+
+        LocalDirectoryDependency f = mockWithName(LocalDirectoryDependency, 'f')
+        when(vendorSnapshoter.isUpToDate(f, new File(resource, 'vendor/f'))).thenReturn(false)
+
+        ResolvedDependency b2 = mockResolvedDependency('github.com/user/b')
+
+        GolangDependencySet buildSet = asGolangDependencySet(a, b)
+        GolangDependencySet testSet = asGolangDependencySet(b2, f)
+        when(golangTaskContainer.get(ResolveBuildDependenciesTask).getFlatDependencies()).thenReturn(buildSet)
+        when(golangTaskContainer.get(ResolveTestDependenciesTask).getFlatDependencies()).thenReturn(testSet)
 
         // when
-        installBuildDependenciesTask.installDependencies()
+        task.installDependenciesToVendor()
 
         // then
-        verify(a, times(0)).installTo(new File(resource, 'src/github.com/user/a'))
-        verify(b).installTo(new File(resource, 'src/github.com/user/b'))
-        assert IOUtils.toString(new File(resource, 'src/github.com/user/b/.CURRENT_VERSION')) == 'b'
-        verify(c).installTo(new File(resource, 'src/github.com/user/c'))
-        assert IOUtils.toString(new File(resource, 'src/github.com/user/c/.CURRENT_VERSION')) == 'c'
-        verify(d).installTo(new File(resource, 'src/github.com/user/d'))
-        assert IOUtils.toString(new File(resource, 'src/github.com/user/d/.CURRENT_VERSION')) == 'd'
+        verify(a, times(0)).installTo(new File(resource, 'vendor/github.com/user/a'))
+        assert !new File(resource, 'vendor/github.com/user/a/vendor').exists()
+        assert new File(resource, 'vendor/github.com/user/a/a.go').exists()
 
-        assert !new File(resource, 'src/github.com/user/e').exists()
+        verify(b).installTo(new File(resource, 'vendor/github.com/user/b'))
+        verify(b2, times(0)).installTo(new File(resource, 'vendor/github.com/user/b'))
+        assert IOUtils.dirIsEmpty(new File(resource, 'vendor/github.com/user/b'))
 
-        verify(f).installTo(new File(resource, 'src/f'))
-        assert IOUtils.toString(new File(resource, 'src/f/.CURRENT_VERSION')) == 'f'
-        verify(g).installTo(new File(resource, 'src/g'))
-        assert IOUtils.toString(new File(resource, 'src/g/.CURRENT_VERSION')) == 'g'
+        assert !new File(resource, 'vendor/github.com/user/c').exists()
+
+        verify(f).installTo(new File(resource, 'vendor/f'))
+        assert IOUtils.dirIsEmpty(new File(resource, 'vendor/f'))
+    }
+
+    @Test
+    void 'files under vendor should be deleted'() {
+        // given
+        when(golangTaskContainer.get(ResolveBuildDependenciesTask).getFlatDependencies()).thenReturn(GolangDependencySet.empty())
+        when(golangTaskContainer.get(ResolveTestDependenciesTask).getFlatDependencies()).thenReturn(GolangDependencySet.empty())
+        IOUtils.write(resource, 'vendor/vendor.json', '')
+        // when
+        task.installDependenciesToVendor()
+        // then
+        assert !new File(resource, 'vendor/vendor.json').exists()
     }
 }
