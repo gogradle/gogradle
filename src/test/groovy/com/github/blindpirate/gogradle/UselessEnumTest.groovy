3b89aa27170964720874bd00f55e77cd806abe7e
Bo Zhang <zhangbodut@gmail.com>
2017-05-18 18:56:32 +0800

    Remove changed enum
    Signed-off-by: Bo Zhang <zhangbodut@gmail.com>

--- src/test/groovy/com/github/blindpirate/gogradle/UselessEnumTest.groovy
+++ src/test/groovy/com/github/blindpirate/gogradle/UselessEnumTest.groovy
@@ -35,7 +35,6 @@ class UselessEnumTest {
          Arch,
          CacheScope,
          AbstractNotationDependency.NoTransitivePredicate,
-         DependencyInstallFileFilter,
          GogradleGlobal].each(this.&doUselessTest)
     }
 
