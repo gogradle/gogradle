0aa03a22177c0c651148469e693f93dd2e6b6c93
Bo Zhang <zhangbodut@gmail.com>
2017-06-09 16:38:38 +0800

    Rename goTest to test in Windows build
    Signed-off-by: Bo Zhang <zhangbodut@gmail.com>

--- src/integration-test/groovy/com/github/blindpirate/gogradle/GogsBuild.groovy
+++ src/integration-test/groovy/com/github/blindpirate/gogradle/GogsBuild.groovy
@@ -63,7 +63,7 @@ vet {
 
         // I don't know why it will fail on Windows
         if (Os.getHostOs() == Os.WINDOWS) {
-            writeBuildAndSettingsDotGradle(buildDotGradle + 'goTest.enabled = false\n')
+            writeBuildAndSettingsDotGradle(buildDotGradle + 'test.enabled = false\n')
         } else {
             writeBuildAndSettingsDotGradle(buildDotGradle)
         }
