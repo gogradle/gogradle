7781dde013637ab16f12bb55f8d1cc3f5957ab57
Bo Zhang <zhangbodut@gmail.com>
2017-07-24 06:57:23 +0800

    Move showGopathGorootTask to another group
    Signed-off-by: Bo Zhang <zhangbodut@gmail.com>

--- src/main/java/com/github/blindpirate/gogradle/task/ShowGopathGorootTask.java
+++ src/main/java/com/github/blindpirate/gogradle/task/ShowGopathGorootTask.java
@@ -36,7 +36,6 @@ public class ShowGopathGorootTask extends AbstractGolangTask {
     private BuildManager buildManager;
 
     public ShowGopathGorootTask() {
-        setGroup("IDE");
         setDescription("Show GOPATH and GOROOT in build.");
         dependsOn(PREPARE_TASK_NAME);
     }
