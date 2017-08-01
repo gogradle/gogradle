50770623c4e624b86051f6b01c877f5d8f67e084
fedor <fedor.korotkov@gmail.com>
2017-08-01 13:35:41 -0700

    Deterministic order of locked dependencies
    I've noticed that when I'm adding a new dependency and trying to re-generate my lock file it's getting messed up. All deps are in a new random order.
    
    This change makes dependencies in `gogradle.lock` always be sorted by name alphabetically which will make reviewing diffs for such changes much easier.

--- src/main/java/com/github/blindpirate/gogradle/core/dependency/lock/DefaultLockedDependencyManager.java
+++ src/main/java/com/github/blindpirate/gogradle/core/dependency/lock/DefaultLockedDependencyManager.java
@@ -17,6 +17,7 @@
 
 package com.github.blindpirate.gogradle.core.dependency.lock;
 
+import com.github.blindpirate.gogradle.core.dependency.GolangDependency;
 import com.github.blindpirate.gogradle.core.dependency.ResolvedDependency;
 import com.github.blindpirate.gogradle.core.dependency.produce.ExternalDependencyFactory;
 import com.github.blindpirate.gogradle.util.DataExchange;
@@ -27,6 +28,7 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.io.File;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -62,6 +64,7 @@ public class DefaultLockedDependencyManager extends ExternalDependencyFactory im
 
     private List<Map<String, Object>> toNotations(Collection<? extends ResolvedDependency> flatDependencies) {
         List<Map<String, Object>> ret = flatDependencies.stream()
+                .sorted(Comparator.comparing(GolangDependency::getName)) // to have a deterministic order
                 .map(ResolvedDependency::toLockedNotation)
                 .collect(Collectors.toList());
         ret.forEach(this::deactivateTransitive);
