e340c5ca7ed4876b6d028d4e1336b05383079e9e
Bo Zhang <bo@gradle.com>
2017-12-12 20:40:15 +0800

    Add cross version tests

---
+++ src/cross-version-test/kotlin/com/github/blindpirate/gogradle/CrossVersionSmokeTest.kt
@@ -0,0 +1,50 @@
+package com.github.blindpirate.gogradle
+
+import com.github.blindpirate.gogradle.GogradleRunner
+import com.github.blindpirate.gogradle.support.IntegrationTestSupport
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.File
+import com.github.blindpirate.gogradle.support.WithResource
+import com.github.blindpirate.gogradle.task.go.GoCoverTest
+import org.gradle.testkit.runner.GradleRunner
+
+/*
+ * Copyright 2016-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+@WithResource("go-test-cover")
+@RunWith(GogradleRunner::class)
+class CrossVersionSmokeTest : IntegrationTestSupport() {
+    override fun getProjectRoot(): File {
+        return resource
+    }
+
+    @Test
+    fun crossVersionTestShouldSucceed() {
+        super.writeBuildAndSettingsDotGradle(buildDotGradleBase)
+
+        listOf("3.5.1", "4.0.1", "4.1", "4.2.1", "4.3.1", "4.4").forEach {
+            GradleRunner.create()
+                    .withProjectDir(resource)
+                    .withArguments("cover", "test", "--info")
+                    .withGradleVersion(it)
+                    .forwardOutput()
+                    .build()
+            GoCoverTest.examineCoverageHtmls(resource)
+        }
+    }
+}
