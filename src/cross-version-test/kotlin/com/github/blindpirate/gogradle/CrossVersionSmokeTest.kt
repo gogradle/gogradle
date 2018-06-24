5ee4294cd98fd6792c76c9276f80ba2d93ab8e3f
Bo Zhang <bo@gradle.com>
2018-06-24 17:07:51 +0800

    Remove --no-daemon

--- src/cross-version-test/kotlin/com/github/blindpirate/gogradle/CrossVersionSmokeTest.kt
+++ src/cross-version-test/kotlin/com/github/blindpirate/gogradle/CrossVersionSmokeTest.kt
@@ -32,8 +32,10 @@ class CrossVersionSmokeTest : IntegrationTestSupport() {
     companion object {
         @JvmField
         public val VERSIONS = listOf(
-                 "3.5.1",
-                "4.0.1", "4.1", "4.2.1", "4.3.1",
+//                 "3.5.1",
+//                "4.0.1",
+//                "4.1",
+                "4.2.1", "4.3.1",
                 "4.4.1", "4.5.1", "4.6", "4.7", "4.8.1")
     }
 
@@ -48,7 +50,7 @@ class CrossVersionSmokeTest : IntegrationTestSupport() {
         VERSIONS.forEach {
             GradleRunner.create()
                     .withProjectDir(resource)
-                    .withArguments("cover", "test", "--info", "--no-daemon")
+                    .withArguments("cover", "test", "--info")
                     .withGradleVersion(it)
                     .forwardOutput()
                     .build()
