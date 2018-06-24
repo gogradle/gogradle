2bcb73eb3e4aa02c78404c2b59202c823979e8d0
Bo Zhang <bo@gradle.com>
2018-06-24 16:36:57 +0800

    Increase cache-compatible version to 0.10

--- src/main/java/com/github/blindpirate/gogradle/GogradleGlobal.java
+++ src/main/java/com/github/blindpirate/gogradle/GogradleGlobal.java
@@ -23,9 +23,9 @@ import org.gradle.api.Project;
 
 public enum GogradleGlobal {
     INSTANCE;
-
+    CrossVersionSmokeTest
     public static final String GOGRADLE_VERSION = "0.9.2";
-    public static final String GOGRADLE_COMPATIBLE_VERSION = "0.8";
+    public static final String GOGRADLE_COMPATIBLE_VERSION = "0.10";
     public static final String DEFAULT_CHARSET = "UTF-8";
     public static final String GOGRADLE_BUILD_DIR_NAME = ".gogradle";
     public static final int MAX_DFS_DEPTH = 100;
