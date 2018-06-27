70b7121df3c970c2d4a4efa36decd14b7ee3c89b
Bo Zhang <bo@gradle.com>
2018-06-27 22:00:42 +0800

    Fix incorrect merge

--- src/main/java/com/github/blindpirate/gogradle/GogradleModule.java
+++ src/main/java/com/github/blindpirate/gogradle/GogradleModule.java
@@ -121,7 +121,6 @@ public class GogradleModule extends AbstractModule {
         bind(GoBinaryManager.class).to(DefaultGoBinaryManager.class);
         bind(MapNotationParser.class).to(DefaultMapNotationParser.class);
         bind(GlobalCacheManager.class).to(DefaultGlobalCacheManager.class);
-        bind(DependencyRegistry.class).to(DefaultDependencyRegistry.class);
         bind(NotationConverter.class).to(DefaultNotationConverter.class);
         bind(BuildConstraintManager.class).to(DefaultBuildConstraintManager.class);
         bind(DependencyVisitor.class).to(DefaultDependencyVisitor.class);
