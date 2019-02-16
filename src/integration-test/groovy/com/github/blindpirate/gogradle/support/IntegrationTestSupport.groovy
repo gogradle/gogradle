b41d65645add466caff5aa19ed5799f6f64fdca4
Bo Zhang <bo@gradle.com>
2019-02-16 18:06:16 +0800

    Print build.gradle

--- src/integration-test/groovy/com/github/blindpirate/gogradle/support/IntegrationTestSupport.groovy
+++ src/integration-test/groovy/com/github/blindpirate/gogradle/support/IntegrationTestSupport.groovy
@@ -54,6 +54,7 @@ golang {
     packagePath = 'github.com/my/project'
 }
 """
+        println("build.gradle content: $buildDotGradleBase")
         if (userhome != null) {
             buildDotGradleBase = "System.setProperty('gradle.user.home','${StringUtils.toUnixString(userhome)}')" + buildDotGradleBase
         }
